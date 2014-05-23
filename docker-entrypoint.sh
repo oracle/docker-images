b9f1fde6cd24a2c398209644e5947af8f138b04e
Tianon Gravi <admwiggin@gmail.com>
2014-05-23 15:58:43 -0600

    Update docker-entrypoint.sh with more readable whitespace

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,12 +14,13 @@ if [ -z "$(ls -A /var/lib/mysql)" -a "${1%_safe}" = 'mysqld' ]; then
 	# These statements _must_ be on individual lines, and _must_ end with
 	# semicolons (no line breaks or comments are permitted).
 	# TODO proper SQL escaping on dat root password D:
-	cat > /tmp/mysql-first-time.sql <<EOF
-UPDATE mysql.user SET host = "%", password = PASSWORD("${MYSQL_ROOT_PASSWORD}") WHERE user = "root" LIMIT 1 ;
-DELETE FROM mysql.user WHERE user != "root" OR host != "%" ;
-DROP DATABASE IF EXISTS test ;
-FLUSH PRIVILEGES ;
-EOF
+	cat > /tmp/mysql-first-time.sql <<-EOSQL
+		UPDATE mysql.user SET host = "%", password = PASSWORD("${MYSQL_ROOT_PASSWORD}") WHERE user = "root" LIMIT 1 ;
+		DELETE FROM mysql.user WHERE user != "root" OR host != "%" ;
+		DROP DATABASE IF EXISTS test ;
+		FLUSH PRIVILEGES ;
+	EOSQL
+	
 	exec "$@" --init-file=/tmp/mysql-first-time.sql
 fi
 
