4aa2080c73719fdcce777ac42791ba9d3d89254d
Tianon Gravi <admwiggin@gmail.com>
2014-08-14 17:34:55 -0600

    Add simple update.sh script

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+for version in "${versions[@]}"; do
+	fullVersion="$(curl -sSL "https://dev.mysql.com/downloads/mysql/$version.html?os=2" \
+		| grep '">(mysql-'"$version"'.*-linux-glibc2\.5-x86_64\.tar\.gz)<' \
+		| sed -r 's!.*\(mysql-([^<)]+)-linux-glibc2\.5-x86_64\.tar\.gz\).*!\1!' \
+		| sort -V | tail -1)"
+	
+	(
+		set -x
+		sed -ri '
+			s/^(ENV MYSQL_MAJOR) .*/\1 '"$version"'/;
+			s/^(ENV MYSQL_VERSION) .*/\1 '"$fullVersion"'/
+		' "$version/Dockerfile"
+	)
+done
