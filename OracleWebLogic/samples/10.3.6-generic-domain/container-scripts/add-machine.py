cb2969ff32e6b92a79da47efb541bd2cc342f58e
Bruno Borges <bruno.borges@gmail.com>
2015-02-12 02:19:32 -0200

    improved scripts for 11g

--- OracleWebLogic/samples/10.3.6-generic-domain/container-scripts/add-machine.py
+++ OracleWebLogic/samples/10.3.6-generic-domain/container-scripts/add-machine.py
@@ -1,19 +1,28 @@
+#
 # Script to add NodeManager automatically to the domain's AdminServer running on 'wlsadmin'.
 #
-# - wlsadmin: name of the linked Docker container with AdminServer running on
+# Since: October, 2014
+# Author: bruno.borges@oracle.com
+#
 # =============================
 import socket
 import os
-machine_name = os.environ['DOCKER_CONTAINER_NAME']
-listen_address = socket.gethostbyname(socket.gethostname())
-connect('weblogic',os.environ["ADMIN_PASSWORD"], os.environ["ADMIN_ADDRESS"])
+
+username = os.getenv('ADMIN_USERNAME', 'weblogic')
+password = os.environ.get("ADMIN_PASSWORD")
+adminurl = os.environ.get("ADMIN_URL", 't3://wlsadmin:7001')
+machinename = os.environ.get('CONTAINER_NAME', "nodemanager_" + socket.gethostname())
+listenaddress = os.environ.get('NM_HOST', socket.gethostbyname(socket.gethostname()))
+listenport = os.environ.get('NM_PORT', '5556')
+
+connect(username, password, adminurl)
 edit()
 startEdit()
 cd('/')
-cmo.createMachine(machine_name)
-cd('/Machines/' + machine_name +'/NodeManager/' + machine_name)
-cmo.setListenPort(int(os.environ["NM_PORT"]))
-cmo.setListenAddress(listen_address)
+cmo.createMachine(machinename)
+cd('/Machines/' + machinename +'/NodeManager/' + machinename)
+cmo.setListenPort(int(listenport))
+cmo.setListenAddress(listenaddress)
 save()
 activate()
 exit()
