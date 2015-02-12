4a32803a9b822b815d551661eefe4f933141da50
Bruno Borges <bruno.borges@gmail.com>
2015-02-12 02:18:35 -0200

    improved magic scripts to start and add NM to admin domain

--- OracleWebLogic/samples/12.1.3-generic-domain/container-scripts/add-machine.py
+++ OracleWebLogic/samples/12.1.3-generic-domain/container-scripts/add-machine.py
@@ -1,26 +1,28 @@
+#
 # Script to add NodeManager automatically to the domain's AdminServer running on 'wlsadmin'.
 #
-# - DOCKER_CONTAINER_NAME: unique name for this container
-#   Give a value with $ docker run -e DOCKER_CONTAINER_NAME=some_unique_name
-# - ADMIN_PASSWORD: provided during image build. See Dockerfile sample
-# - NM_PORT: provided during image build. See Dockerfile sample
-# - ADMIN_ADDRESS: t3 URL of an AdminServer running somewhere.
-#   Inform this value as $ docker run -e ADMIN_ADDRESS=t3//some_ip:some_port
-# 
-# Both containers, the one with NM and the one with AdminServer, must be able to communicate bidirectionally.
+# Since: October, 2014
+# Author: bruno.borges@oracle.com
+#
 # =============================
 import socket
 import os
-machine_name = os.environ['DOCKER_CONTAINER_NAME']
-listen_address = socket.gethostbyname(socket.gethostname())
-connect('weblogic',os.environ["ADMIN_PASSWORD"], os.environ["ADMIN_ADDRESS"])
+
+username = os.getenv('ADMIN_USERNAME', 'weblogic')
+password = os.environ.get("ADMIN_PASSWORD")
+adminurl = os.environ.get("ADMIN_URL", 't3://wlsadmin:7001')
+machinename = os.environ.get('CONTAINER_NAME', "nodemanager_" + socket.gethostname())
+listenaddress = os.environ.get('NM_HOST', socket.gethostbyname(socket.gethostname()))
+listenport = os.environ.get('NM_PORT', '5556')
+
+connect(username, password, adminurl)
 edit()
 startEdit()
 cd('/')
-cmo.createMachine(machine_name)
-cd('/Machines/' + machine_name +'/NodeManager/' + machine_name)
-cmo.setListenPort(int(os.environ["NM_PORT"]))
-cmo.setListenAddress(listen_address)
+cmo.createMachine(machinename)
+cd('/Machines/' + machinename +'/NodeManager/' + machinename)
+cmo.setListenPort(int(listenport))
+cmo.setListenAddress(listenaddress)
 save()
 activate()
 exit()
--- OracleWebLogic/samples/12.1.3-generic-domain/container-scripts/createMachine.sh
+++ OracleWebLogic/samples/12.1.3-generic-domain/container-scripts/createMachine.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# Set WLS domain environment
-. /u01/oracle/weblogic/user_projects/domains/base_domain/bin/setDomainEnv.sh
-
-# TODO: check if machine already exists. If it does, make sure it listen to the correct ip address before start NM.
-# if not, then add after start NM.
-
 # Start Node Manager
-nohup /u01/oracle/weblogic/user_projects/domains/base_domain/bin/startNodeManager.sh &
+nohup startNodeManager.sh > log.nm &
 
-# Add if necessary
-sleep 5 && java -Dweblogic.security.SSL.ignoreHostnameVerification=true weblogic.WLST \
-	-skipWLSModuleScanning \
-	/u01/oracle/add-machine.py
-while true; do
-  sleep 60
-done
+# Wait and add it to the AdminServer
+sleep 5 && . addMachine.sh
+
+# print log 
+tail -f log.nm
