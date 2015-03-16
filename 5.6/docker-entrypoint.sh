9080c6a2af21e90e75020d4183b41d373d6e0d8f
Lars Tangvald <lars.tangvald@oracle.com>
2015-03-16 09:32:54 +0100

    * Fix whitespace inconsistencies * Removed hardcoded path from my_print_defaults command * Moved command for setting datadir ownership to before server is started

--- 5.6/docker-entrypoint.sh
+++ 5.6/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 set -e
 
 get_option () {
-    local section=$1
-    local option=$2
-    local default=$3
-    ret=$(/usr/bin/my_print_defaults $section | grep '^--'${option}'=' | cut -d= -f2-)
-    [ -z $ret ] && ret=$default
-    echo $ret
+	local section=$1
+	local option=$2
+	local default=$3
+	ret=$(my_print_defaults $section | grep '^--'${option}'=' | cut -d= -f2-)
+	[ -z $ret ] && ret=$default
+	echo $ret
 }
 
 if [ "${1:0:1}" = '-' ]; then
@@ -17,8 +17,8 @@ fi
 if [ "$1" = 'mysqld' ]; then
 	# Get config
 	DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
-        SOCKET=$(get_option  mysqld socket "$datadir/mysql.sock")
-        PIDFILE=$(get_option mysqld pid-file "/var/run/mysqld/mysqld.pid")
+	SOCKET=$(get_option  mysqld socket "$datadir/mysql.sock")
+	PIDFILE=$(get_option mysqld pid-file "/var/run/mysqld/mysqld.pid")
 
 	if [ ! -d "$DATADIR/mysql" ]; then
 		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
@@ -29,25 +29,27 @@ if [ "$1" = 'mysqld' ]; then
 		if [ ! -d "$DATADIR" ]; then
 			mkdir -p $DATADIR
 		fi
+		chown -R mysql:mysql "$DATADIR"
+
 		echo 'Running mysql_install_db'
 		mysql_install_db --user=mysql --datadir=$DATADIR --rpm --keep-my-cnf
 		echo 'Finished mysql_install_db'
 
 		mysqld --user=mysql --datadir=$DATADIR --skip-networking &
-                for i in $(seq 30 -1 0); do
-		    [ -S $SOCKET ] && break
-		    echo 'MySQL init process in progress...'
-		    sleep 1
+		for i in $(seq 30 -1 0); do
+			[ -S $SOCKET ] && break
+			echo 'MySQL init process in progress...'
+			sleep 1
 		done
-                if [ $i = 0 ]; then
-                    echo >&2 'MySQL init process failed.'
-                    exit 1
-                fi
-                
+		if [ $i = 0 ]; then
+			echo >&2 'MySQL init process failed.'
+			exit 1
+		fi
+
 		# These statements _must_ be on individual lines, and _must_ end with
 		# semicolons (no line breaks or comments are permitted).
 		# TODO proper SQL escaping on ALL the things D:
-		
+
 		tempSqlFile=$(mktemp /tmp/mysql-first-time.XXXXXX.sql)
 		cat > "$tempSqlFile" <<-EOSQL
 			DELETE FROM mysql.user ;
@@ -55,35 +57,34 @@ if [ "$1" = 'mysqld' ]; then
 			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
 			DROP DATABASE IF EXISTS test ;
 		EOSQL
-		
+
 		if [ "$MYSQL_DATABASE" ]; then
 			echo "CREATE DATABASE IF NOT EXISTS \`"$MYSQL_DATABASE"\` ;" >> "$tempSqlFile"
 		fi
-		
+
 		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
 			echo "CREATE USER '"$MYSQL_USER"'@'%' IDENTIFIED BY '"$MYSQL_PASSWORD"' ;" >> "$tempSqlFile"
-			
+
 			if [ "$MYSQL_DATABASE" ]; then
 				echo "GRANT ALL ON \`"$MYSQL_DATABASE"\`.* TO '"$MYSQL_USER"'@'%' ;" >> "$tempSqlFile"
 			fi
 		fi
-		
+
 		echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile"
-		
-		chown -R mysql:mysql "$DATADIR"
+
 		mysql -uroot < $tempSqlFile
 
 		rm -f $tempSqlFile
 		kill $(cat $PIDFILE)
-                for i in $(seq 30 -1 0); do
-		    [ -S $SOCKET ] || break
-		    echo 'MySQL init process in progress...'
-		    sleep 1
+		for i in $(seq 30 -1 0); do
+			[ -S $SOCKET ] || break
+			echo 'MySQL init process in progress...'
+			sleep 1
 		done
-                if [ $i = 0 ]; then
-                    echo >&2 'MySQL hangs during init process.'
-                    exit 1
-                fi
+		if [ $i = 0 ]; then
+			echo >&2 'MySQL hangs during init process.'
+			exit 1
+		fi
 		echo 'MySQL init process done. Ready for start up.'
 	fi
 
