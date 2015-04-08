de4fd357e9758b60ef8387fcbda4a55329b5d9cf
Juan C. Fernández <juan.carlos.fernandez@ricoh.es>
2015-04-08 16:16:03 +0200

    Restore weblogic 10.3.6 docker configuration. (From original timeline)

--- OracleWebLogic/dockerfiles/buildDockerImage.sh
+++ OracleWebLogic/dockerfiles/buildDockerImage.sh
@@ -6,45 +6,33 @@
 
 usage() {
 cat << EOF
-Usage: buildDockerImage.sh [-d | -g]
+Usage: buildDockerImage.sh -v version [-d]
 Builds a Docker Image for WebLogic.
   
 Parameters:
-   -d: creates image based on 'developer' distribution
-   -g: creates image based on 'generic' distribution
-
-* use either -d or -g, obligatory.
+   -v: version to build. Required.
+       Choose one of: $(for i in $(ls -d */); do echo -n "${i%%/}  "; done)
+   -d: creates image based on 'developer' distribution, or 'generic' if ommitted.
 
 LICENSE CDDL 1.0 + GPL 2.0
 EOF
 exit 0
 }
 
-# Validate packages
-checksumPackages() {
-  echo "Checking if required packages are present and valid..."
-  md5sum -c Checksum.$DISTRIBUTION
-  if [ "$?" -ne 0 ]; then
-    echo "MD5 for required packages to build this image did not match!"
-    exit $?
-  fi
-}
+if [ "$#" -eq 0 ]; then
+    usage
+fi
 
-if [ "$#" -eq 0 ]; then usage; fi
-
-# Parameters
-DEVELOPER=0
-GENERIC=0
-while getopts "hdg" optname; do
+while getopts "hdfv:" optname; do
   case "$optname" in
     "h")
       usage
       ;;
-    "d")
-      DEVELOPER=1
+    "v")
+      VERSION="$OPTARG"
       ;;
-    "g")
-      GENERIC=1
+    "d")
+      DEVELOPER=true
       ;;
     *)
     # Should not occur
@@ -53,24 +41,41 @@ while getopts "hdg" optname; do
   esac
 done
 
-# WebLogic Image Names and Version
-VERSION="12.1.3"
+if [ -z "$VERSION" ]; then
+  echo "You must inform a version to be built."
+  echo "Example: ./buildDockerImage.sh -v 12.1.3"
+  echo ""
+  echo "Versions available:"
+  for i in $(ls -d */); do echo " - ${i%%/}"; done
+  exit 1
+fi
+
+# WebLogic Image Names
 DEFAULT_IMAGE_NAME="oracle/weblogic:$VERSION"
 DEFAULT_DEV_IMAGE_NAME="$DEFAULT_IMAGE_NAME-dev"
 
 # Developer or Generic?
-if [ "$DEVELOPER" -eq "$GENERIC" ]; then
-  usage
-elif [ $DEVELOPER -eq 1 ]; then
+if [ $DEVELOPER ]; then
   DISTRIBUTION="developer"
   IMAGE_NAME="$DEFAULT_DEV_IMAGE_NAME"
 else
   DISTRIBUTION="generic"
   IMAGE_NAME="$DEFAULT_IMAGE_NAME"
 fi
-
+  
 # Go into version folder
 cd $VERSION
+  
+# Validate packages
+checksumPackages() {
+  echo "Checking if required packages are present and valid..."
+  md5sum -c Checksum.$DISTRIBUTION
+  if [ "$?" -ne 0 ]
+  then
+    echo "MD5 for required packages to build this image did not match!"
+    exit $?
+  fi
+}
 
 checksumPackages
 
@@ -84,7 +89,7 @@ echo "Building image '$IMAGE_NAME' based on '$DISTRIBUTION' distribution..."
 # BUILD THE IMAGE (replace all environment variables)
 rm -f Dockerfile && ln -s Dockerfile.$DISTRIBUTION Dockerfile
 docker build --force-rm=true --no-cache=true --rm=true -t $IMAGE_NAME . 
-rm -f Dockerfile
+rm Dockerfile
 
 if [ $? -ne 0 ]; then
   echo "There was an error building the image."
@@ -92,7 +97,6 @@ if [ $? -ne 0 ]; then
 fi
 
 echo ""
-
 if [ $? -eq 0 ]; then
   echo "WebLogic Docker Image for '$DISTRIBUTION' $VERSION is ready to be extended: $IMAGE_NAME"
 else
