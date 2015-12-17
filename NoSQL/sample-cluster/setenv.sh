d936d53b16e53fa6066934a54fd352a61a5221fc
Bruno Borges <bruno.borges@gmail.com>
2015-12-17 14:48:16 -0800

    removed unnecessary echos

--- NoSQL/sample-cluster/setenv.sh
+++ NoSQL/sample-cluster/setenv.sh
@@ -4,8 +4,3 @@ orchestrator_address=$(docker-machine ip $orchestrator)
 registry="$orchestrator_address:5000"
 consul="$orchestrator_address:8500"
 network=$prefix-net
-
-echo "orchestrator=$orchestrator"
-echo "registry=$registry"
-echo "consul=$consul"
-echo "network=$network"
