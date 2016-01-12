5dee767d7d8b11719fbd21bbd067514b70166c26
Bruno Borges <bruno.borges@gmail.com>
2016-01-12 12:06:18 -0800

    fixed name of machine

--- OracleWebLogic/samples/1221-domain/container-scripts/add-server.py
+++ OracleWebLogic/samples/1221-domain/container-scripts/add-server.py
@@ -35,7 +35,7 @@ nmname = os.environ.get('NM_NAME', 'Machine-' + socket.gethostname())
 
 # ManagedServer details
 msinternal = socket.gethostbyname(socket.gethostname())
-msname = os.environ.get('MS_NAME', 'ManagedServer-' randomName() + '@' socket.gethostname())
+msname = os.environ.get('MS_NAME', 'ManagedServer-' + randomName() + '@' socket.gethostname())
 mshost = os.environ.get('MS_HOST', socket.gethostbyname(socket.gethostname()))
 msport = os.environ.get('MS_PORT', '7001')
 memargs = os.environ.get('USER_MEM_ARGS', '')
