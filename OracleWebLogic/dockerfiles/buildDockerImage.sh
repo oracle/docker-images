8c07638aacc2bd5827abcbd9547b16c2a63c3965
Bruno Borges <bruno.borges@gmail.com>
2016-01-27 16:44:15 -0800

    prints out build elapsed time

--- OracleWebLogic/dockerfiles/buildDockerImage.sh
+++ OracleWebLogic/dockerfiles/buildDockerImage.sh
@@ -113,15 +113,25 @@ echo "====================="
 echo "Building image '$IMAGE_NAME' ..."
 
 # BUILD THE IMAGE (replace all environment variables)
+BUILD_START=$(date '+%s')
 docker build --force-rm=true --no-cache=true -t $IMAGE_NAME -f Dockerfile.$DISTRIBUTION . || {
   echo "There was an error building the image."
   exit 1
 }
+BUILD_END=$(date '+%s')
+BUILD_ELAPSED=`expr $BUILD_END - $BUILD_START`
 
 echo ""
 
 if [ $? -eq 0 ]; then
-  echo "WebLogic Docker Image for '$DISTRIBUTION' version $VERSION is ready to be extended: $IMAGE_NAME"
+cat << EOF
+  WebLogic Docker Image for '$DISTRIBUTION' version $VERSION is ready to be extended: 
+    
+    --> $IMAGE_NAME
+
+  Build completed in $BUILD_ELAPSED seconds.
+
+EOF
 else
   echo "WebLogic Docker Image was NOT successfully created. Check the output and correct any reported problems with the docker build operation."
 fi
