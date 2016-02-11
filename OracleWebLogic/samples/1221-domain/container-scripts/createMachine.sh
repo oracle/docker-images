b2039c341483b3e1f9caf3868407e1c3694397f9
Bruno Borges <bruno.borges@gmail.com>
2016-02-11 15:20:35 -0800

    check for admin server on specific port instead of using sleep

--- OracleWebLogic/samples/1221-domain/container-scripts/createMachine.sh
+++ OracleWebLogic/samples/1221-domain/container-scripts/createMachine.sh
@@ -5,12 +5,25 @@
 CONFIG_JVM_ARGS="${CONFIG_JVM_ARGS} -Dweblogic.security.SSL.ignoreHostnameVerification=true"
 WLST="wlst.sh -skipWLSModuleScanning"
 
-# Start Node Manager
-nohup startNodeManager.sh > log.nm &
-sleep 5
+# If log.nm does not exists, container is starting for 1st time
+# So it should start NM and also associate with AdminServer
+# Otherwise, only start NM (container restarted)
+if [ ! -f log.nm ]; then
+    ADD_MACHINE=1
+fi
 
-# Add a Machine to the AdminServer
-$WLST /u01/oracle/add-machine.py
+# Start Node Manager
+echo "Starting NodeManager in background..."
+nohup startNodeManager.sh > log.nm 2>&1 &
+echo "NodeManager started."
+
+# Wait for AdminServer to become available for any subsequent operation
+./waitForAdminServer.sh
+
+# Add a Machine to the AdminServer only if 1st execution
+if [ $ADD_MACHINE -eq 1 ]; then
+  $WLST /u01/oracle/add-machine.py
+fi
 
 # print log
 tail -f log.nm
--- OracleWebLogic/samples/1221-domain/container-scripts/createServer.sh
+++ OracleWebLogic/samples/1221-domain/container-scripts/createServer.sh
@@ -5,15 +5,26 @@
 CONFIG_JVM_ARGS="${CONFIG_JVM_ARGS} -Dweblogic.security.SSL.ignoreHostnameVerification=true"
 WLST="wlst.sh -skipWLSModuleScanning"
 
+# If log.nm does not exists, container is starting for 1st time
+# So it should start NM and also associate with AdminServer, as well Managed Server
+# Otherwise, only start NM (container is being restarted)
+if [ ! -f log.nm ]; then
+    ADD_SERVER=1
+fi
+
 # Start Node Manager
-nohup startNodeManager.sh > log.nm &
-sleep $DELAY_NM_REGISTRATION
+echo "Starting NodeManager in background..."
+nohup startNodeManager.sh > log.nm 2>&1 &
+echo "NodeManager started."
 
-# Add a Machine to the AdminServer
-$WLST /u01/oracle/add-machine.py
+# Wait for AdminServer to become available for any subsequent operation
+./waitForAdminServer.sh
 
-# Wait and add a new Managed Server
-$WLST /u01/oracle/add-server.py
+# Add this 'Machine' and 'ManagedServer' to the AdminServer only if 1st execution
+if [ $ADD_SERVER -eq 1 ]; then
+  $WLST /u01/oracle/add-machine.py
+  $WLST /u01/oracle/add-server.py
+fi
 
 # print log
-tail -f log.nm
+tail -f log.nm /u01/oracle/user_projects/domains/$DOMAIN_NAME/servers/*/logs/*.out
