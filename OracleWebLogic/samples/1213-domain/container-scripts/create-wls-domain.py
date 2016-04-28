82f9cc8729e2b62a6842bd83ed55873b092a7368
Bruno Borges <bruno.borges@gmail.com>
2016-04-27 19:33:31 -0700

    assign jax-rs to adminserver, and do not create jms resources in basedomain

--- OracleWebLogic/samples/1213-domain/container-scripts/create-wls-domain.py
+++ OracleWebLogic/samples/1213-domain/container-scripts/create-wls-domain.py
@@ -32,60 +32,6 @@ set('ListenPort', admin_port)
 cd('/Security/' + domain_name + '/User/weblogic')
 cmo.setPassword(admin_pass)
 
-# Create a JMS Server
-# ===================
-cd('/')
-create('myJMSServer', 'JMSServer')
-
-# Create a JMS System resource
-# ============================
-cd('/')
-create('myJmsSystemResource', 'JMSSystemResource')
-cd('JMSSystemResource/myJmsSystemResource/JmsResource/NO_NAME_0')
-
-# Create a JMS Queue and its subdeployment
-# ========================================
-myq = create('myQueue','Queue')
-myq.setJNDIName('jms/myqueue')
-myq.setSubDeploymentName('myQueueSubDeployment')
-
-cd('/JMSSystemResource/myJmsSystemResource')
-create('myQueueSubDeployment', 'SubDeployment')
-
-# Create and configure a JDBC Data Source, and sets the JDBC user
-# ===============================================================
-cd('/')
-create('myDataSource', 'JDBCSystemResource')
-cd('JDBCSystemResource/myDataSource/JdbcResource/myDataSource')
-create('myJdbcDriverParams','JDBCDriverParams')
-cd('JDBCDriverParams/NO_NAME_0')
-set('DriverName','org.apache.derby.jdbc.ClientDriver')
-set('URL','jdbc:derby://localhost:1527/db;create=true')
-set('PasswordEncrypted', 'PBPUBLIC')
-set('UseXADataSourceInterface', 'false')
-create('myProps','Properties')
-cd('Properties/NO_NAME_0')
-create('user', 'Property')
-cd('Property/user')
-cmo.setValue('PBPUBLIC')
-
-cd('/JDBCSystemResource/myDataSource/JdbcResource/myDataSource')
-create('myJdbcDataSourceParams','JDBCDataSourceParams')
-cd('JDBCDataSourceParams/NO_NAME_0')
-set('JNDIName', java.lang.String("myDataSource_jndi"))
-
-cd('/JDBCSystemResource/myDataSource/JdbcResource/myDataSource')
-create('myJdbcConnectionPoolParams','JDBCConnectionPoolParams')
-cd('JDBCConnectionPoolParams/NO_NAME_0')
-set('TestTableName','SYSTABLES')
-
-# Target resources to the servers 
-# ===============================
-cd('/')
-assign('JMSServer', 'myJMSServer', 'Target', 'AdminServer')
-assign('JMSSystemResource.SubDeployment', 'myJmsSystemResource.myQueueSubDeployment', 'Target', 'myJMSServer')
-assign('JDBCSystemResource', 'myDataSource', 'Target', 'AdminServer')
-
 # Write the domain and close the domain template
 # ==============================================
 setOption('OverwriteDomain', 'true')
@@ -121,6 +67,7 @@ closeTemplate()
 readDomain(domain_path)
 addTemplate('/u01/oracle/jaxrs2-template.jar')
 assign('Library', 'jax-rs#2.0@2.5.1', 'Target', cluster_name)
+assign('Library', 'jax-rs#2.0@2.5.1', 'Target', 'AdminServer')
 updateDomain()
 closeDomain()
 
