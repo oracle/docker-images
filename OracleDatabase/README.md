afb42fb02dc92b59f0d8b2e5fda5df1778a039c5
Gerald Venzl <gerald.venzl@gmail.com>
2016-07-15 15:44:52 -0700

    Docker build files for Oracle Database (#118)
    * Container starting successfully
    
    * Oracle DB EE working
    
    * Including build scrip
    
    buildDockerImage.sh can now be used to drive a Docker build. It also
    includes checksum verification of the downloaded files.
    
    * Enable PDB OPEN at startup
    
    * Fixed LISTENER unknown service
    
    * Changes in README.md
    
    * Fixed broken link
    
    * Cleanup install binaries right after install
    
    * Removed yum upgrade
    
    * Provide admin account pwd, rename image
    
    It is now possible to provide a password for the admin accounts for the
    Database (SYS, SYSTEM, PDBAMIN). In case that no password has been
    specified the build script will generate one.
    
    The image has been renamed to oracle/database:<version>-<edition>
    
    * Update README.md with running instructions
    
    * Fixed typo
    
    * Include doc and steps on how to connect
    
    * Include OEM URL
    
    * Update README.md
    
    * Update buildDockerImage.sh
    
    * Update Dockerfile.ee
    
    * Decrease layers in docker file

---
+++ OracleDatabase/README.md
@@ -0,0 +1,69 @@
+Oracle Database on Docker
+===============
+Sample Docker build files to facilitate installation, configuration, and environment setup for DevOps users. For more information about Oracle Database please see the [Oracle Database Online Documentation](http://docs.oracle.com/database/121/index.htm).
+
+## How to build and run
+This project offers sample Dockerfiles for both Oracle Database 12c (12.1.0.2) Enterprise Edition and Standard Edition. To assist in building the images, you can use the [buildDockerImage.sh](dockerfiles/buildDockerImage.sh) script. See below for instructions and usage.
+
+The `buildDockerImage.sh` script is just a utility shell script that performs MD5 checks and is an easy way for beginners to get started. Expert users are welcome to directly call `docker build` with their prefered set of parameters.
+
+### Building Oracle Database Docker Install Images
+**IMPORTANT:** You will have to provide the installation binaries of Oracle Database and put them into the `dockerfiles/<version>` folder. You only need to provide the binaries for the edition you are going to install.
+
+Before you build the image make sure that you have provided the installation binaries and put them into the right folder. Once you have chosen which edition and version you want to build an image of, go into the **dockerfiles** folder and run the **buildDockerImage.sh** script as root or with `sudo` privileges:
+
+	[oracle@localhost dockerfiles]$ ./buildDockerImage.sh -h
+	
+	Usage: buildDockerImage.sh -v [version] [-e | -s | -x] [-p] [-i]
+	Builds a Docker Image for Oracle Database.
+	
+	Parameters:
+	   -v: version to build
+	       Choose one of: 11.2.0.2  12.1.0.2
+	   -e: creates image based on 'Enterprise Edition'
+	   -s: creates image based on 'Standard Edition 2'
+	   -x: creates image based on 'Express Edition'
+	   -p: password for database admin accounts (it will be generated if omitted)
+	   -i: ignores the MD5 checksums
+	
+	* select one edition only: -e, -s, or -x
+	
+	LICENSE CDDL 1.0 + GPL 2.0
+	
+	Copyright (c) 2014-2016 Oracle and/or its affiliates. All rights reserved.
+
+**IMPORTANT:** The resulting images will be an newly installed Oracle Database. You may extend the image with your own Dockerfile and create the users and tablespaces that you may need.
+
+### Running Oracle Database in a Docker container
+To run your Oracle Database Docker image just use the **docker run** command as follows:
+
+	docker run -p 1521:1521 -p 5500:5500 oracle/database:12.1.0.2-ee
+
+There are two ports that are exposed in this image:
+* 1521 which is the port to connect to the Oracle Database.
+* 5500 which is the port of Oracle Enterprise Manager Express.
+
+The admin accounts created are:
+* system (SYSDBA for ORCLCDB)
+* pdbadmin (ORCLPDB1)
+
+Once the container has been started you can connect to it just like to any other database:
+
+	sqlplus system/<your password>@//localhost:1521/ORCLCDB
+	sqlplus pdbadmin/<your password>@//localhost:1521/ORCLPDB1
+	
+You may use the same Docker image to run `sqlplus`, for example:
+
+        docker run --rm -ti oracle/database:12.1.0.2-ee sqlplus pdbadmin/<yourpassword>@//<db-container-ip>:1521/ORCLPDB1
+
+The Oracle Database inside the container also has Oracle Enterprise Manager Express configured. To access OEM Express, start your browser and follow the URL:
+
+	https://localhost:5500/em/
+
+## License
+To download and run Oracle Database, regardless whether inside or outside a Docker container, you must download the binaries from the Oracle website and accept the license indicated at that page.
+
+All scripts and files hosted in this project and GitHub [docker-images/OracleDatabase](./) repository required to build the Docker images are, unless otherwise noted, released under the Common Development and Distribution License (CDDL) 1.0 and GNU Public License 2.0 licenses.
+
+## Copyright
+Copyright (c) 2014-2016 Oracle and/or its affiliates. All rights reserved.
---
+++ OracleDatabase/dockerfiles/buildDockerImage.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+# 
+# Since: April, 2016
+# Author: gerald.venzl@oracle.com
+# Description: Build script for building Oracle Database Docker images.
+# 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
+# Copyright (c) 2014-2016 Oracle and/or its affiliates. All rights reserved.
+# 
+
+usage() {
+cat << EOF
+
+Usage: buildDockerImage.sh -v [version] [-e | -s | -x] [-p] [-i]
+Builds a Docker Image for Oracle Database.
+  
+Parameters:
+   -v: version to build
+       Choose one of: $(for i in $(ls -d */); do echo -n "${i%%/}  "; done)
+   -e: creates image based on 'Enterprise Edition'
+   -s: creates image based on 'Standard Edition 2'
+   -x: creates image based on 'Express Edition'
+   -p: password for database admin accounts (it will be generated if omitted)
+   -i: ignores the MD5 checksums
+
+* select one edition only: -e, -s, or -x
+
+LICENSE CDDL 1.0 + GPL 2.0
+
+Copyright (c) 2014-2016 Oracle and/or its affiliates. All rights reserved.
+
+EOF
+exit 0
+}
+
+# Validate packages
+checksumPackages() {
+  echo "Checking if required packages are present and valid..."
+  md5sum -c Checksum.$EDITION
+  if [ "$?" -ne 0 ]; then
+    echo "MD5 for required packages to build this image did not match!"
+    echo "Make sure to download missing files in folder $VERSION."
+    exit $?
+  fi
+}
+
+if [ "$#" -eq 0 ]; then usage; fi
+
+# Parameters
+ENTERPRISE=0
+STANDARD=0
+EXPRESS=0
+VERSION="12.1.0.2"
+SKIPMD5=0
+DOCKEROPS=""
+ORACLE_PWD=""
+GENERATED_PWD=1
+
+while getopts "hesxivp:" optname; do
+  case "$optname" in
+    "h")
+      usage
+      ;;
+    "i")
+      SKIPMD5=1
+      ;;
+    "e")
+      ENTERPRISE=1
+      ;;
+    "s")
+      STANDARD=1
+      ;;
+    "x")
+      EXPRESS=1
+      ;;
+    "v")
+      VERSION="$OPTARG"
+      ;;
+    "p")
+      ORACLE_PWD="$OPTARG"
+      GENERATED_PWD=0
+      ;;
+    *)
+    # Should not occur
+      echo "Unknown error while processing options inside buildDockerImage.sh"
+      ;;
+  esac
+done
+
+# Which Edition should be used?
+if [ $((ENTERPRISE + STANDARD + EXPRESS)) -gt 1 ]; then
+  usage
+elif [ $ENTERPRISE -eq 1 ]; then
+  EDITION="ee"
+elif [ $STANDARD -eq 1 ]; then
+  EDITION="se2"
+elif [ $EXPRESS -eq 1 ] && [ "$VERSION" = "12.1.0.2" ]; then
+  echo "Version 12.1.0.2 does not have Express Edition available."
+  exit 1
+else
+  EDITION="xe";
+  DOCKEROPS="--shm-size=1G";
+fi
+
+# Is password omitted?
+if [ "$GENERATED_PWD" -eq 1 ]; then
+   ORACLE_PWD="`pwmake 64`";
+fi
+
+# Oracle Database Image Name
+IMAGE_NAME="oracle/database:$VERSION-$EDITION"
+
+# Go into version folder
+cd $VERSION
+
+if [ ! "$SKIPMD5" -eq 1 ]; then
+  checksumPackages
+else
+  echo "Ignored MD5 checksum."
+fi
+
+echo "====================="
+
+# Proxy settings
+PROXY_SETTINGS=""
+if [ "${http_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg=\"http_proxy=${http_proxy}\""
+fi
+
+if [ "${https_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg=\"https_proxy=${https_proxy}\""
+fi
+
+if [ "${ftp_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg=\"ftp_proxy=${ftp_proxy}\""
+fi
+
+if [ "${no_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg=\"no_proxy=${no_proxy}\""
+fi
+
+if [ "$PROXY_SETTINGS" != "" ]; then
+  echo "Proxy settings were found and will be used during build."
+fi
+
+# ################## #
+# BUILDING THE IMAGE #
+# ################## #
+echo "Building image '$IMAGE_NAME' ..."
+
+# BUILD THE IMAGE (replace all environment variables)
+BUILD_START=$(date '+%s')
+docker build --build-arg ORACLE_PWD=$ORACLE_PWD --force-rm=true --no-cache=true $DOCKEROPS $PROXY_SETTINGS -t $IMAGE_NAME -f Dockerfile.$EDITION . || {
+  echo "There was an error building the image."
+  exit 1
+}
+BUILD_END=$(date '+%s')
+BUILD_ELAPSED=`expr $BUILD_END - $BUILD_START`
+
+echo ""
+
+if [ $? -eq 0 ]; then
+cat << EOF
+  Oracle Database Docker Image for '$EDITION' version $VERSION is ready to be extended: 
+    
+    --> $IMAGE_NAME
+    
+EOF
+  if [ "$GENERATED_PWD" -eq 1 ]; then
+    cat <<EOF
+  GENERATED PASSWORD (SYS, SYSTEM, PDBADMIN): $ORACLE_PWD
+
+EOF
+  fi
+
+cat << EOF
+  Build completed in $BUILD_ELAPSED seconds.
+  
+EOF
+
+else
+  echo "Oracle Database Docker Image was NOT successfully created. Check the output and correct any reported problems with the docker build operation."
+fi
+
