9f4af9118657d0b207e6aeaae8c799b4e055266c
Gerald Venzl <gerald.venzl@gmail.com>
2016-08-15 14:29:29 -0700

    Startup DB on container restart

--- OracleDatabase/dockerfiles/11.2.0.2/runOracle.sh
+++ OracleDatabase/dockerfiles/11.2.0.2/runOracle.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
- # Auto generate ORACLE PWD
-ORACLE_PWD=`openssl rand -hex 8`
-echo "ORACLE AUTO GENERATED PASSWORD FOR SYS and SYSTEM: $ORACLE_PWD";
+############# Create DB ################
+function createDB {
+   # Auto generate ORACLE PWD
+   ORACLE_PWD=`openssl rand -hex 8`
+   echo "ORACLE AUTO GENERATED PASSWORD FOR SYS AND SYSTEM: $ORACLE_PWD";
 
-sed -i -e "s|###ORACLE_PWD###|$ORACLE_PWD|g" $ORACLE_BASE/$CONFIG_RSP && \
-/etc/init.d/oracle-xe configure responseFile=$ORACLE_BASE/$CONFIG_RSP
+   sed -i -e "s|###ORACLE_PWD###|$ORACLE_PWD|g" $ORACLE_BASE/$CONFIG_RSP && \
+   /etc/init.d/oracle-xe configure responseFile=$ORACLE_BASE/$CONFIG_RSP
 
-# Listener 
-echo "LISTENER = \
+   # Listener 
+   echo "LISTENER = \
   (DESCRIPTION_LIST = \
     (DESCRIPTION = \
       (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC_FOR_XE)) \
@@ -18,12 +20,19 @@ echo "LISTENER = \
 \
 " > $ORACLE_HOME/network/admin/listener.ora
 
-echo "DEDICATED_THROUGH_BROKER_LISTENER=ON"  >> $ORACLE_HOME/network/admin/listener.ora && \
-echo "DIAG_ADR_ENABLED = off"  >> $ORACLE_HOME/network/admin/listener.ora;
+   echo "DEDICATED_THROUGH_BROKER_LISTENER=ON"  >> $ORACLE_HOME/network/admin/listener.ora && \
+   echo "DIAG_ADR_ENABLED = off"  >> $ORACLE_HOME/network/admin/listener.ora;
 
-
-su -p oracle -c "sqlplus / as sysdba <<EOF
-EXEC DBMS_XDB.SETLISTENERLOCALACCESS(FALSE);
+   su -p oracle -c "sqlplus / as sysdba <<EOF
+      EXEC DBMS_XDB.SETLISTENERLOCALACCESS(FALSE);
 EOF"
+}
+
+############# MAIN ################
+
+/etc/init.d/oracle-xe start | grep -qc "Oracle Database 11g Express Edition is not configured"
+if [ "$?" == "0" ]; then
+   createDB;
+fi;
 
 tail -f $ORACLE_BASE/diag/rdbms/*/*/trace/alert*.log
