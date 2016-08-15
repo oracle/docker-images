c4a001b0b21435dc4fd61ffe0f8bfe4e4a6b76e0
Gerald Venzl <gerald.venzl@gmail.com>
2016-08-15 12:23:59 -0700

    Create DB at container startup

--- OracleDatabase/dockerfiles/11.2.0.2/runOracle.sh
+++ OracleDatabase/dockerfiles/11.2.0.2/runOracle.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-lsnrctl start
-sqlplus / as sysdba <<EOF
-startup;
+ # Auto generate ORACLE PWD
+ORACLE_PWD=`openssl rand -hex 8`
+echo "ORACLE AUTO GENERATED PASSWORD FOR SYS and SYSTEM: $ORACLE_PWD";
+
+sed -i -e "s|###ORACLE_PWD###|$ORACLE_PWD|g" $ORACLE_BASE/$CONFIG_RSP && \
+/etc/init.d/oracle-xe configure responseFile=$ORACLE_BASE/$CONFIG_RSP
+
+# Listener 
+echo "LISTENER = \
+  (DESCRIPTION_LIST = \
+    (DESCRIPTION = \
+      (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC_FOR_XE)) \
+      (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521)) \
+    ) \
+  ) \
+\
+" > $ORACLE_HOME/network/admin/listener.ora
+
+echo "DEDICATED_THROUGH_BROKER_LISTENER=ON"  >> $ORACLE_HOME/network/admin/listener.ora && \
+echo "DIAG_ADR_ENABLED = off"  >> $ORACLE_HOME/network/admin/listener.ora;
+
+
+su -p oracle -c "sqlplus / as sysdba <<EOF
 EXEC DBMS_XDB.SETLISTENERLOCALACCESS(FALSE);
-EOF
+EOF"
 
 tail -f $ORACLE_BASE/diag/rdbms/*/*/trace/alert*.log
