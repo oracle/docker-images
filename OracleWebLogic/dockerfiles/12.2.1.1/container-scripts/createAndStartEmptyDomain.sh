23fcbcb24e9e9578a9c04d5c315d38b8029dec4b
Bruno Borges <bruno.borges@gmail.com>
2016-09-21 12:03:14 -0700

    skip module scanning on wlst to faster bootup

--- OracleWebLogic/dockerfiles/12.2.1.1/container-scripts/createAndStartEmptyDomain.sh
+++ OracleWebLogic/dockerfiles/12.2.1.1/container-scripts/createAndStartEmptyDomain.sh
@@ -2,25 +2,54 @@
 #
 # Copyright (c) 2014-2015 Oracle and/or its affiliates. All rights reserved.
 #
+########### SIGTERM handler ############
+function _term() {
+   echo "Stopping container."
+   echo "SIGTERM received, shutting down database!"
+   sqlplus / as sysdba <<EOF
+   shutdown immediate;
+EOF
+   lsnrctl stop
+}
+
+########### SIGKILL handler ############
+function _kill() {
+   echo "SIGKILL received, shutting down database!"
+   sqlplus / as sysdba <<EOF
+   shutdown abort;
+EOF
+   lsnrctl stop
+}
+
+# Set SIGTERM handler
+trap _term SIGTERM
+
+# Set SIGKILL handler
+trap _kill SIGKILL
+
 # Auto generate Oracle WebLogic Server admin password
 ADMIN_PASSWORD=$(cat /dev/urandom | tr -dc '0-9A-Z' | fold -w 8 | head -n 1)
-echo "Oracle WebLogic Server auto generated Admin password:" $ADMIN_PASSWORD
-
+echo ""
+echo "    Oracle WebLogic Server Auto Generated Admin password:"
+echo ""
+echo "      ----> $ADMIN_PASSWORD"
+echo ""
 
 sed -i -e "s|ADMIN_PASSWORD|$ADMIN_PASSWORD|g" /u01/oracle/create-wls-domain.py
 
-# Create the empty domain
-wlst.sh /u01/oracle/create-wls-domain.py
+# Create an empty domain
+wlst.sh -skipWLSModuleScanning /u01/oracle/create-wls-domain.py
 mkdir -p ${DOMAIN_HOME}/servers/AdminServer/security/ 
 echo "username=weblogic" > /u01/oracle/user_projects/domains/$DOMAIN_NAME/servers/AdminServer/security/boot.properties 
 echo "password=$ADMIN_PASSWORD" >> /u01/oracle/user_projects/domains/$DOMAIN_NAME/servers/AdminServer/security/boot.properties 
 ${DOMAIN_HOME}/bin/setDomainEnv.sh 
 
-#Start Admin Server and tail the logs
-
+# Start Admin Server and tail the logs
 ${DOMAIN_HOME}/startWebLogic.sh
 touch ${DOMAIN_HOME}/servers/AdminServer/logs/AdminServer.log
-tail -f ${DOMAIN_HOME}/servers/AdminServer/logs/AdminServer.log
+tail -f ${DOMAIN_HOME}/servers/AdminServer/logs/AdminServer.log &
 
+childPID=$!
+wait $childPID
 
 
