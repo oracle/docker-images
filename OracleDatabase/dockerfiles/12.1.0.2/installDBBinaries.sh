18d2a7fce38d0b15b21a02c8d2965f5ba2d58494
Gerald Venzl <gerald.venzl@gmail.com>
2016-12-11 15:00:23 +0000

    Simplified Dockerfiles and DB Install

---
+++ OracleDatabase/dockerfiles/12.1.0.2/installDBBinaries.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# LICENSE CDDL 1.0 + GPL 2.0
+#
+# Copyright (c) 1982-2016 Oracle and/or its affiliates. All rights reserved.
+#
+# Since: December, 2016
+# Author: gerald.venzl@oracle.com
+# Description: Sets up the unix environment for DB installation.
+# 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
+
+EDITION=$1
+
+# Check whether edition has been passed on
+if [ "$EDITION" == "" ]; then
+   echo "ERROR: No edition has been passed on!"
+   echo "Please specify the correct edition!"
+   exit 1;
+fi;
+
+# Check whether correct edition has been passed on
+if [ "$EDITION" != "EE" -a "$EDITION" != "SE2" ]; then
+   echo "ERROR: Wrong edition has been passed on!"
+   echo "Edition $EDITION is no a valid edition!"
+   exit 1;
+fi;
+
+# Check whether ORACLE_BASE is set
+if [ "$ORACLE_BASE" == "" ]; then
+   echo "ERROR: ORACLE_BASE has not been set!"
+   echo "You have to have the ORACLE_BASE environment variable set to a valid value!"
+   exit 1;
+fi;
+
+# Check whether ORACLE_HOME is set
+if [ "$ORACLE_HOME" == "" ]; then
+   echo "ERROR: ORACLE_HOME has not been set!"
+   echo "You have to have the ORACLE_HOME environment variable set to a valid value!"
+   exit 1;
+fi;
+
+
+# Replace place holders
+# ---------------------
+sed -i -e "s|###ORACLE_EDITION###|$EDITION|g" $INSTALL_DIR/$INSTALL_RSP && \
+sed -i -e "s|###ORACLE_BASE###|$ORACLE_BASE|g" $INSTALL_DIR/$INSTALL_RSP && \
+sed -i -e "s|###ORACLE_HOME###|$ORACLE_HOME|g" $INSTALL_DIR/$INSTALL_RSP && \
+cd $INSTALL_DIR       && \
+unzip $INSTALL_FILE_1 && \
+rm $INSTALL_FILE_1    && \
+unzip $INSTALL_FILE_2 && \
+rm $INSTALL_FILE_2    && \
+$INSTALL_DIR/database/runInstaller -silent -force -waitforcompletion -responsefile $INSTALL_DIR/$INSTALL_RSP -ignoresysprereqs -ignoreprereq && \
+rm -rf $INSTALL_DIR/database && \
+ln -s $ORACLE_BASE/$PWD_FILE $HOME/ && \
+echo "DEDICATED_THROUGH_BROKER_LISTENER=ON"  >> $ORACLE_HOME/network/admin/listener.ora && \
+echo "DIAG_ADR_ENABLED = off"  >> $ORACLE_HOME/network/admin/listener.ora;
+
+# Check whether Perl is working
+chmod ug+x $INSTALL_DIR/installPerl.sh && \
+$ORACLE_HOME/perl/bin/perl -v || \
+$INSTALL_DIR/installPerl.sh
