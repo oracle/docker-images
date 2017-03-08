f1e4f57871c322928c4e55caa10c2d937d5496da
Jorge Quilcate <quilcate.jorge@gmail.com>
2017-03-08 10:41:48 +0100

    refactor domain scripts

--- OracleSOA/samples/12212-osb-compact-domain/container-scripts/create-osb-domain.py
+++ OracleSOA/samples/12212-osb-compact-domain/container-scripts/create-osb-domain.py
@@ -1,3 +1,4 @@
+#!/usb/bin/python
 # Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
 #
 # OSB on Docker Default Compact Domain
@@ -9,14 +10,21 @@
 # ==============================================
 domain_name  = os.environ.get("DOMAIN_NAME", "base_domain")
 admin_port   = int(os.environ.get("ADMIN_PORT", "8001"))
-admin_pass   = os.environ.get("ADMIN_PASSWORD")
-cluster_name = os.environ.get("CLUSTER_NAME", "DockerCluster")
-domain_path  = '/u01/oracle/user_projects/domains/' + domain_name
-app_path     = '/u01/oracle/user_projects/applications/' + domain_name
+admin_pass   = "ADMIN_PASSWORD"
+domain_path  = '/u01/oracle/user_projects/domains/%s' % domain_name
+production_mode = os.environ.get("PRODUCTION_MODE", "prod")
+
+print('domain_name : [%s]' % domain_name);
+print('admin_port  : [%s]' % admin_port);
+print('domain_path : [%s]' % domain_path);
+print('production_mode : [%s]' % production_mode);
 
 # Open default domain template
 # ======================
 readTemplate("/u01/oracle/wlserver/common/templates/wls/wls.jar", 'Compact')
+
+set('Name', domain_name)
+setOption('DomainName', domain_name)
 
 # Disable Admin Console
 # --------------------
@@ -30,13 +38,13 @@ set('ListenPort', admin_port)
 
 # Define the user password for weblogic
 # =====================================
-cd('/Security/' + domain_name + '/User/weblogic')
+cd('/Security/%s/User/weblogic' % domain_name)
 cmo.setPassword(admin_pass)
 
 # Write the domain and close the domain template
 # ==============================================
 setOption('OverwriteDomain', 'true')
-setOption('ServerStartMode','prod')
+setOption('ServerStartMode',production_mode)
 
 cd('/NMProperties')
 set('ListenAddress','')
@@ -47,32 +55,26 @@ set('StartScriptEnabled', 'false')
 set('SecureListener', 'false')
 set('LogLevel', 'FINEST')
 
-# Set the Node Manager user name and password
-cd('/SecurityConfiguration/' + domain_name)
+# Set the Node Manager user name and password (domain name will change after writeDomain)
+cd('/SecurityConfiguration/base_domain')
 set('NodeManagerUsername', 'weblogic')
 set('NodeManagerPasswordEncrypted', admin_pass)
 
-# Define a WebLogic Cluster
-# =========================
-cd('/')
-create(cluster_name, 'Cluster')
-
-cd('/Clusters/' + cluster_name)
-cmo.setClusterMessagingMode('unicast')
-
-# Write Domain
-# ============
 writeDomain(domain_path)
 closeTemplate()
+
 
 # Add OSB template
 # ================
 readDomain(domain_path)
 addTemplate('/u01/oracle/osb/common/templates/wls/oracle.osb_template.jar')
 
-# Update Domain
+
+# Write Domain
 # ============
 updateDomain()
-closeDomain();
+closeDomain()
 
+# Exit WLST
+# =========
 exit()
