ec5f67a356674b855f650dc39c013ad14356539e
Monica Riccelli <monica.riccelli@oracle.com>
2017-03-14 19:20:49 -0400

    Change 1213-domain to accept PRODUCTION_MODE as argument

--- OracleWebLogic/samples/1213-domain/container-scripts/create-wls-domain.py
+++ OracleWebLogic/samples/1213-domain/container-scripts/create-wls-domain.py
@@ -11,11 +11,21 @@ domain_name  = os.environ.get("DOMAIN_NAME", "base_domain")
 admin_port   = int(os.environ.get("ADMIN_PORT", "7001"))
 admin_pass   = os.environ.get("ADMIN_PASSWORD")
 cluster_name = os.environ.get("CLUSTER_NAME", "DockerCluster")
-domain_path  = '/u01/oracle/user_projects/domains/' + domain_name
+domain_path  = '/u01/oracle/user_projects/domains/%s' % domain_name
+production_mode         = os.environ.get("PRODUCTION_MODE", "prod")
+
+print('domain_name : [%s]' % domain_name);
+print('admin_port  : [%s]' % admin_port);
+print('cluster_name: [%s]' % cluster_name);
+print('domain_path : [%s]' % domain_path);
+print('production_mode : [%s]' % production_mode);
 
 # Open default domain template
 # ======================
 readTemplate("/u01/oracle/wlserver/common/templates/wls/wls.jar")
+
+set('Name', domain_name)
+setOption('DomainName', domain_name)
 
 # Disable Admin Console
 # --------------------
@@ -29,13 +39,13 @@ set('ListenPort', admin_port)
 
 # Define the user password for weblogic
 # =====================================
-cd('/Security/' + domain_name + '/User/weblogic')
+cd('/Security/%s/User/weblogic' % domain_name)
 cmo.setPassword(admin_pass)
 
 # Write the domain and close the domain template
 # ==============================================
 setOption('OverwriteDomain', 'true')
-setOption('ServerStartMode','prod')
+setOption('ServerStartMode', production_mode)
 
 cd('/NMProperties')
 set('ListenAddress','')
@@ -46,8 +56,8 @@ set('StartScriptEnabled', 'false')
 set('SecureListener', 'false')
 set('LogLevel', 'FINEST')
 
-# Set the Node Manager user name and password
-cd('/SecurityConfiguration/' + domain_name)
+# Set the Node Manager user name and password (domain name will change after writeDomain)
+cd('/SecurityConfiguration/base_domain')
 set('NodeManagerUsername', 'weblogic')
 set('NodeManagerPasswordEncrypted', admin_pass)
 
@@ -56,22 +66,13 @@ set('NodeManagerPasswordEncrypted', admin_pass)
 cd('/')
 create(cluster_name, 'Cluster')
 
-cd('/Clusters/' + cluster_name)
+cd('/Clusters/%s' % cluster_name)
 cmo.setClusterMessagingMode('unicast')
 
 # Write Domain
 # ============
 writeDomain(domain_path)
 closeTemplate()
-
-# Enable JAX-RS 2.0 by default on this domain
-# ===========================================
-readDomain(domain_path)
-addTemplate('/u01/oracle/jaxrs2-template.jar')
-assign('Library', 'jax-rs#2.0@2.5.1', 'Target', cluster_name)
-assign('Library', 'jax-rs#2.0@2.5.1', 'Target', 'AdminServer')
-updateDomain()
-closeDomain()
 
 # Exit WLST
 # =========
