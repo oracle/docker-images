ba777addddc15546411f5d4d31458b54ee97015c
Monica RIccelli <monica.riccelli@oracle.com>
2017-03-14 15:37:56 -0400

    Changes to createAndStartEmptyDomain and remove chpasswd (#308)
    * Changes to createAndStartEmptyDomain and remove chpasswd
    
    * Add SIGTERM and SIGKILL for WebLogic
    
    * Changes to useradd for permissions to /u01/oracle and remove ctrl-M character

--- OracleWebLogic/dockerfiles/12.2.1.1/container-scripts/createAndStartEmptyDomain.sh
+++ OracleWebLogic/dockerfiles/12.2.1.1/container-scripts/createAndStartEmptyDomain.sh
@@ -2,23 +2,20 @@
 #
 # Copyright (c) 2014-2015 Oracle and/or its affiliates. All rights reserved.
 #
+# If AdminServer.log does not exists, container is starting for 1st time
+# So it should start NM and also associate with AdminServer
+# Otherwise, only start NM (container restarted)
 ########### SIGTERM handler ############
 function _term() {
    echo "Stopping container."
-   echo "SIGTERM received, shutting down database!"
-   sqlplus / as sysdba <<EOF
-   shutdown immediate;
-EOF
-   lsnrctl stop
+   echo "SIGTERM received, shutting down the server!"
+   ${DOMAIN_HOME}/bin/stopWebLogic.sh
 }
 
 ########### SIGKILL handler ############
 function _kill() {
-   echo "SIGKILL received, shutting down database!"
-   sqlplus / as sysdba <<EOF
-   shutdown abort;
-EOF
-   lsnrctl stop
+   echo "SIGKILL received, shutting down the server!"
+   kill -9 $childPID
 }
 
 # Set SIGTERM handler
@@ -27,9 +24,6 @@ trap _term SIGTERM
 # Set SIGKILL handler
 trap _kill SIGKILL
 
-# If AdminServer.log does not exists, container is starting for 1st time
-# So it should start NM and also associate with AdminServer
-# Otherwise, only start NM (container restarted)
 ADD_DOMAIN=1
 if [ ! -f ${DOMAIN_HOME}/servers/AdminServer/logs/AdminServer.log ]; then
     ADD_DOMAIN=0
@@ -38,7 +32,7 @@ fi
 # Create Domain only if 1st execution
 if [ $ADD_DOMAIN -eq 0 ]; then
 # Auto generate Oracle WebLogic Server admin password
-ADMIN_PASSWORD=$(cat date| md5sum | fold -w 8 | head -n 1) 
+ADMIN_PASSWORD=$(date| md5sum | fold -w 8 | head -n 1) 
 
 echo ""
 echo "    Oracle WebLogic Server Auto Generated Empty Domain:"
