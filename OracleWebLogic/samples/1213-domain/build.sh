523d3affb6a7f92444eb4d027ceab17a6fed3ad0
Monica Riccelli <monica.riccelli@oracle.com>
2017-03-14 20:59:42 -0400

    Correct echo

--- OracleWebLogic/samples/1213-domain/build.sh
+++ OracleWebLogic/samples/1213-domain/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-if [ "$#" -eq 0 ]; then echo "Inform a password for the domain as first argument."; exit; fi
+if [ "$#" -eq 0 ]; then echo "Provide a password for the domain as first argument."; exit; fi
 docker build --build-arg ADMIN_PASSWORD=$1 --build-arg PRODUCTION_MODE=$2 -t 1213-domain . 
 
