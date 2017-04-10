4e002d754b273a3b030e81a79e3eced101a177bb
Rick Michaud <rmichaud@icloud.com>
2017-04-10 13:37:40 -0400

    Changes to remove unsupported EPEL repository for rlwrap utility and unnecessary Java installation.

--- OracleGoldenGate/buildContainer.sh
+++ OracleGoldenGate/buildContainer.sh
@@ -1,2 +1,55 @@
 #!/bin/bash
-docker run --name oggoradb -h oggoradb -e ORACLE_SID=ORCL -e ORACLE_PDB=pdb1 -e ORACLE_PWD=welcome1 -P ogg-oracle:12.1.0.2-ee
+#
+# Since: April, 2016
+# Author: rick.michaud@oracle.com
+# Description: Build script for building Oracle Database Containers 
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+#
+# Copyright (c) 2014-2017 Oracle and/or its affiliates. All rights reserved.
+#
+
+help () {
+cat << EOF
+	Usage: ${0##*/} [-h] [-C CONTAINER_NAME] [-H HOST]  [-S ORACLE_SID] [-P ORACLE_PDB] [-p PASSWORD]
+
+	This helps to build a container for the Oracle Database and GoldenGate docker image so that multiple environments
+	can be quickly built ready to use.
+
+	-h 		Display this help and exit
+	-C		(Uppercase) Specify the name of the docker container
+	-H 		(Uppercase) Specify the host name of the docker container
+	-S 		(Uppercase) Specify the Oracle Database SID/ServiceName
+	-P		(Uppercase) Specify the Oracle Database Pluggable Database (PDB) Name
+	-p 		(Lowercase) Specify the passwords for the Oracle Database sys/system accounts
+EOF
+exit 1
+}
+
+while getopts :h:C:H:S:P:p: opt; do
+	case ${opt} in
+	h ) 	help
+		;;
+	C )	CONTAINER=${OPTARG}
+		echo "CONTAINER NAME		===> $CONTAINER"
+		;;
+	H )	HOST=${OPTARG}
+		echo "HOST NAME		===> $HOST"
+		;;
+	S )	SID=${OPTARG}
+		echo "ORACLE SID		===> $SID"
+		;;
+	P )	PDB=${OPTARG}
+		echo "ORACLE PDB		===> $PDB" 
+		;;
+	p )	PASSWORD=${OPTARG}
+		echo "PASSWORDS		===> $PASSWORD"
+		;;
+	* )
+		echo "ERROR: Invalid option: \""$opt"\"" >&2
+		help
+	esac
+done
+
+echo "Running: docker run --name $CONTAINER -h $HOST -e ORACLE_SID=$SID -e ORACLE_PDB=$PDB -e ORACLE_PWD=$PASSWORD -P ogg-oracle:12.1.0.2-ee"
+docker run --name $CONTAINER -h $HOST -e ORACLE_SID=$SID -e ORACLE_PDB=$PDB -e ORACLE_PWD=$PASSWORD -P ogg-oracle:12.1.0.2-ee
--- OracleGoldenGate/setupLinuxEnv.sh
+++ OracleGoldenGate/setupLinuxEnv.sh
@@ -13,18 +13,11 @@
 # Adjust file permissions, go to /opt/oracle as user 'oracle' to proceed with Oracle installation
 # ------------------------------------------------------------
 
-# Setup environment for GoldenGate.   Also install rlwrap, vim and Java 1.8 for other requirements.
+# Setup environment for GoldenGate.   
 mkdir -p $OGG_INSTALL_DIR && \
 chown -R oracle:oinstall $INSTALL_DIR && \
 chown -R oracle:oinstall $OGG_INSTALL_DIR && \
 chmod ug+x $INSTALL_DIR && \
 chmod ug+x $OGG_INSTALL_DIR && \
-wget http://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && \
-rpm -Uvh epel-release-latest-7.noarch.rpm && \
-yum -y -q install rlwrap && \
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm" && \
-yum -y -q localinstall jdk-8u60-linux-x64.rpm && \
 yum -y -q install vim-enhanced && \
-yum clean all && \
-rm epel-release-latest-7.noarch.rpm && \
-rm jdk-8u60-linux-x64.rpm
+yum clean all 
