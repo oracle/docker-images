f14faa43a122599f18ee450b23c94008c86c287e
Gerald Venzl <gerald.venzl@gmail.com>
2017-05-08 18:39:56 -0700

    Fix for #307, auto calculate memory on bigger envs

--- OracleDatabase/dockerfiles/12.2.0.1/createDB.sh
+++ OracleDatabase/dockerfiles/12.2.0.1/createDB.sh
@@ -31,6 +31,15 @@ sed -i -e "s|###ORACLE_PDB###|$ORACLE_PDB|g" $ORACLE_BASE/dbca.rsp
 sed -i -e "s|###ORACLE_PWD###|$ORACLE_PWD|g" $ORACLE_BASE/dbca.rsp
 sed -i -e "s|###ORACLE_CHARACTERSET###|$ORACLE_CHARACTERSET|g" $ORACLE_BASE/dbca.rsp
 
+# If there is greater than 8 CPUs default back to dbca memory calculations
+# dbca will automatically pick 40% of available memory for Oracle DB
+# The minimum of 2G is for small environments to guarantee that Oracle has enough memory to function
+# However, bigger environment can and should use more of the available memory
+# This is due to Github Issue #307
+if [ `nproc` -gt 8 ]; then
+   sed -i -e "s|totalMemory=2048||g" $ORACLE_BASE/dbca.rsp
+fi;
+
 # Create network related config files (sqlnet.ora, tnsnames.ora, listener.ora)
 mkdir -p $ORACLE_HOME/network/admin
 echo "NAME.DIRECTORY_PATH= {TNSNAMES, EZCONNECT, HOSTNAME}" > $ORACLE_HOME/network/admin/sqlnet.ora
