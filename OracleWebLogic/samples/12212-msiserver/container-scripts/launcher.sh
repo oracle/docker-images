c6fc9a6cab84c5ed95da05999f4fabc1220b8151
Aseem Bajaj <aseem.bajaj@oracle.com>
2017-06-02 10:23:29 -0700

    Incorporating feedback from Avi

--- OracleWebLogic/samples/12212-msiserver/container-scripts/launcher.sh
+++ OracleWebLogic/samples/12212-msiserver/container-scripts/launcher.sh
@@ -1,14 +1,17 @@
 #! /usr/bin/bash
 
-domain_home=$1
-ms_name_from_image=$2
-username=$3
-password=$4
-number_of_ms=$5
+ms_name_from_image=${DEFAULT_MS_NAME}
+number_of_ms=${NUMBER_OF_MS}
 ms_name=${MS_NAME:-ms$(( ( RANDOM % $number_of_ms )  + 1 ))}
+domain_home=${DOMAINS_DIR}/${DOMAIN_NAME}
+
+echo "Launching with parameters"
+echo "Domain Home: " $domain_home
+echo "MS Name from Image: " $ms_name_from_image
+echo "MS Name to be used: " $ms_name
+echo "Number of servers configured in image: " $number_of_ms
 
 cd $domain_home
-# echo 'Working in location' `pwd`
 
 # Rename the server directory
 if [ "$ms_name_from_image" != "$ms_name" ]; then
