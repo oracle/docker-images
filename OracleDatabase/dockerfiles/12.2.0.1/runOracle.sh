73f29dd31e256a7bc9898ae30083c73a1483c9f7
JGuzman <susguzman@gmail.com>
2017-06-29 17:00:49 -0600

    Option Container Database

--- OracleDatabase/dockerfiles/12.2.0.1/runOracle.sh
+++ OracleDatabase/dockerfiles/12.2.0.1/runOracle.sh
@@ -125,6 +125,16 @@ else
    fi;
 fi;
 
+# Default for ORACLE CONTAINERDB
+if [ "$ORACLE_CONTAINERDB" == "" ]; then
+   export ORACLE_CONTAINERDB=true
+else
+  if [ "$ORACLE_CONTAINERDB" != "true" || "$ORACLE_CONTAINERDB" != "false" ]; then
+    export ORACLE_CONTAINERDB=true
+    echo "Warning: Database will be create as container database."
+  fi;
+fi;
+
 # Default for ORACLE PDB
 export ORACLE_PDB=${ORACLE_PDB:-ORCLPDB1}
 
