6a87d2eca8ef816b65df7a8820fccc9bfe012e9b
Gerald Venzl <gerald.venzl@gmail.com>
2017-07-26 13:29:01 -0700

    Remove wget install

--- OracleDatabase/dockerfiles/12.1.0.2/setupLinuxEnv.sh
+++ OracleDatabase/dockerfiles/12.1.0.2/setupLinuxEnv.sh
@@ -18,7 +18,7 @@ mkdir $ORACLE_BASE/scripts/startup && \
 ln -s $ORACLE_BASE/scripts /docker-entrypoint-initdb.d && \
 mkdir $ORACLE_BASE/oradata && \
 chmod ug+x $ORACLE_BASE/*.sh && \
-yum -y install oracle-rdbms-server-12cR1-preinstall unzip wget tar openssl && \
+yum -y install oracle-rdbms-server-12cR1-preinstall unzip tar openssl && \
 yum clean all && \
 echo oracle:oracle | chpasswd && \
 chown -R oracle:dba $ORACLE_BASE
