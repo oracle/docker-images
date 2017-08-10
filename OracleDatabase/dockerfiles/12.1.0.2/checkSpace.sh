f2c108cb33ee6f99ac236f01fe5d796335c99574
draikes30 <draikes30@gmail.com>
2017-08-10 16:11:29 -0700

    Issue #532 - Fixed the checkSpace.sh script to force it to report on only the root filesystem of the container. (#533)

--- OracleDatabase/dockerfiles/12.1.0.2/checkSpace.sh
+++ OracleDatabase/dockerfiles/12.1.0.2/checkSpace.sh
@@ -10,13 +10,14 @@
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 # 
 
-REQUIRED_SPACE_GB=12
+REQUIRED_SPACE_GB=15
+AVAILABLE_SPACE_GB=`df -B 1G / | tail -n 1 | awk '{ print $4 }'`
 
-if [ `df -B 1G . | tail -n 1 | awk '{ print $4 }'` -lt $REQUIRED_SPACE_GB ]; then
+if [ $AVAILABLE_SPACE_GB -lt $REQUIRED_SPACE_GB ]; then
   script_name=`basename "$0"`
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   echo "$script_name: ERROR - There is not enough space available in the docker container."
-  echo "$script_name: The container needs at least $REQUIRED_SPACE_GB GB available."
+  echo "$script_name: The container needs at least $REQUIRED_SPACE_GB GB , but only $AVAILABLE_SPACE_GB available."
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   exit 1;
 fi;
