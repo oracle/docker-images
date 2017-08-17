7b706d4dfcb9815bee73489e40c1b0575bf81ac8
PRABHAT KISHORE <prabhat.kishore@oracle.com>
2017-08-18 00:49:06 +0530

    Updated LICENSE and licensing statement. (#544)
    * Deleting startNMandOHS.sh since it is not used.
    
    * Modified LICENSE file and added appropriate licensing statement in every file.

--- OracleHTTPServer/dockerfiles/12.2.1.2.0/container-scripts/configureWLSProxyPlugin.sh
+++ OracleHTTPServer/dockerfiles/12.2.1.2.0/container-scripts/configureWLSProxyPlugin.sh
@@ -3,6 +3,8 @@
 #
 # Copyright (c) 2016-2017 Oracle and/or its affiliates. All rights reserved.
 #
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#
 #*************************************************************************
 #This script will configure Oracle WebLogic Server Proxy Plug-In (mod_wl_ohs),
 #in order to enable the Oracle HTTP Server instances to route applications
