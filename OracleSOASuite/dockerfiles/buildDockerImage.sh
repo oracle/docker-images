14066320d026f26be24c8a551e19d6a9655c0f88
Nevin Cleetus <nevin.cleetus@oracle.com>
2017-08-18 00:50:25 +0530

    Adding UPL 1.0 licensing to all scripts (#542)

--- OracleSOASuite/dockerfiles/buildDockerImage.sh
+++ OracleSOASuite/dockerfiles/buildDockerImage.sh
@@ -4,6 +4,10 @@
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
 # Copyright (c) 2016-2017 Oracle and/or its affiliates. All rights reserved.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#
+
 
 #=============================================================
 usage() {
