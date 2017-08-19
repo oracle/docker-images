378684b832305ca8706ebc23302ef8c5d93bd719
Monica Riccelli <monica.riccelli@oracle.com>
2017-08-19 00:34:29 -0400

    Add UPL 1.0 licensing to all scripts and a COPYRIGHT file at the head directory (#549)

--- OracleWebLogic/samples/12212-domain/container-scripts/createServer.sh
+++ OracleWebLogic/samples/12212-domain/container-scripts/createServer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-# Copyright (c) 2014-2015 Oracle and/or its affiliates. All rights reserved.
+#Copyright (c) 2014-2017 Oracle and/or its affiliates. All rights reserved.
+#
+#Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 #
 export DOMAIN_HOME=/u01/oracle/user_projects/domains/$DOMAIN_NAME
 echo "Domain Home: " $DOMAIN_HOME
