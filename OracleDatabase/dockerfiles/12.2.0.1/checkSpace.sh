7ed45ad98763e5b87c6da943e1c2558b1a993a03
Gerald Venzl <gerald.venzl@gmail.com>
2017-09-18 18:03:21 -0700

    Fix typo in checkSpace.sh script

--- OracleDatabase/dockerfiles/12.2.0.1/checkSpace.sh
+++ OracleDatabase/dockerfiles/12.2.0.1/checkSpace.sh
@@ -17,7 +17,7 @@ if [ $AVAILABLE_SPACE_GB -lt $REQUIRED_SPACE_GB ]; then
   script_name=`basename "$0"`
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   echo "$script_name: ERROR - There is not enough space available in the docker container."
-  echo "$script_name: The container needs at least $REQUIRED_SPACE_GB GB , but only $AVAILABLE_SPACE_GB available."
+  echo "$script_name: The container needs at least $REQUIRED_SPACE_GB GB, but only $AVAILABLE_SPACE_GB GB are available."
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   exit 1;
 fi;
