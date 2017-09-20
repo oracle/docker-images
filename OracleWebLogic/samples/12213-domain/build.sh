5f8d982872743977ee242273471e454f98181b23
Monica Riccelli <monica.riccelli@oracle.com>
2017-09-20 16:19:26 -0400

    WebLogic 12.2.1.3 image and sample 12213 domain (#576)
    * Create sample for the creation of a 12.2.1.3 domain
    * PS3 install image

---
+++ OracleWebLogic/samples/12213-domain/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+#Copyright (c) 2014-2017 Oracle and/or its affiliates. All rights reserved.
+#
+#Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#
+docker build -t 12213-domain . 
