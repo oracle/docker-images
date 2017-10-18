54caf60812d94d3c29cd5ae7edc98f2284e75dd8
Avi Miller <avi.miller@oracle.com>
2017-10-19 10:33:51 +1100

    Updating the Oracle Database scripts to replace yum clean all.
    Signed-off-by: Avi Miller <avi.miller@oracle.com>

--- OracleDatabase/dockerfiles/12.2.0.1/setupLinuxEnv.sh
+++ OracleDatabase/dockerfiles/12.2.0.1/setupLinuxEnv.sh
@@ -19,6 +19,6 @@ ln -s $ORACLE_BASE/scripts /docker-entrypoint-initdb.d && \
 mkdir $ORACLE_BASE/oradata && \
 chmod ug+x $ORACLE_BASE/*.sh && \
 yum -y install oracle-database-server-12cR2-preinstall unzip tar openssl && \
-yum clean all && \
+rm -rf /var/cache/yum && \
 echo oracle:oracle | chpasswd && \
 chown -R oracle:dba $ORACLE_BASE
