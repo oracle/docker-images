03c939d92cfc0a6e7e509e48a653107ac1769099
Dong Han <dong.han@oracle.com>
2018-01-10 16:48:28 +0800

    Add a new Dockerfile for Tuxedo Message Queue (#699)
    * Add a new Dockerfile for Tuxedo Performance Pack
    
    1. Updated OracleTuxedo/README.md for the new Dockerfile
    2. Added a folder for the new Dockerfile.
    
    * Updated according to the comments
    
    1. The pack is available to 12.1.3 and 12.2.2 so use
    oracle/tuxedoall:latest in Dockerfile instead of 12.2.2. The version
    12.2.2 is the latest Tuxedo release.
    2.  Updated README.md and perfpark_runme.sh
    
    * Updated script and README according to the comments
    
    Upated the two shell scripts  and README.md accordingly.
    
    * Updated README.md
    
    Add a section for how to use the image.
    
    * corrected a typo in README.md
    
    * update performance_pack
    
    * add Tuxedo Message Queue docker

---
+++ OracleTuxedo/tuxedo_message_q/dockerfiles/buildDockerImage.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#set -x
+
+usage() {
+cat << EOF
+
+Usage: buildDockerImage.sh [-h] -v version 
+Builds a Docker Image for Oracle Tuxedo.
+  
+Parameters:
+   -h: Help
+   -v: Version to build. Required.
+       Choose one of: $(for i in $(ls -d */); do echo -n "${i%%/}  "; done)
+LICENSE UPL 1.0
+
+Copyright (c) 2016-2017 Oracle and/or its affiliates. All rights reserved.
+
+EOF
+exit 0
+}
+
+
+if [ "$#" -eq 0 ]; then usage; fi
+echo "====================="
+#
+
+VERSION=
+INSTALLER=
+MD5VALUE=
+SKIPMD5=0
+while getopts "h:v:" optname; do
+  case "$optname" in
+    "h")
+      usage
+      ;;
+    "v")
+      VERSION="$OPTARG"
+      ;;
+    *)
+    # Should not occur
+      echo "Unknown error while processing options inside buildDockerImage.sh"
+      ;;
+  esac
+done
+
+if [ -z "$VERSION" ]; then   # -v option is a MUST
+    echo "You must specify Tuxedo Message Queue version with -v option"
+    exit
+fi
+
+echo "====================="
+
+if [ ! -e ${VERSION}/${INSTALLER} ]
+then
+  echo "Download the Tuxedo Message Queue ${VERSION} ZIP Distribution and"
+  echo "drop the file ${INSTALLER} in ${VERSION}/ folder before"
+  echo "building this Tuxedo Message Queue Docker container!"
+  exit 
+fi
+
+# Proxy settings
+PROXY_SETTINGS=""
+if [ "${http_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg http_proxy=${http_proxy}"
+fi
+
+if [ "${https_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg https_proxy=${https_proxy}"
+fi
+
+if [ "${ftp_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg ftp_proxy=${ftp_proxy}"
+fi
+
+if [ "${no_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg no_proxy=${no_proxy}"
+fi
+
+if [ "$PROXY_SETTINGS" != "" ]; then
+  echo "Proxy settings were found and will be used during build."
+fi
+
+echo "====================="
+
+docker build $PROXY_SETTINGS -t oracle/tuxedotmq:${VERSION} ${VERSION}/
+if [ "$?" = "0" ]
+    then
+	echo ""
+	echo "Tuxedo TMQ Docker image is ready to be used. To create a container, run:"
+	echo "docker run -d -v \${LOCAL_DIR}:/u01/oracle/user_projects oracle/tuxedotmq:${VERSION}"
+	echo "Note: \${LOCAL_DIR} is a local dir which used in docker image as external storage, it can be any dir."
+    else
+	echo "Build of Tuxedo TMQ Docker image failed."
+fi
