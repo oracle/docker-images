433c4af1b71a1f678f2e64596434afc887144039
Ashish Chalak <ashish.chalak@oracle.com>
2018-01-19 23:33:26 +0530

    Oracle WebCenter Sites Docker Scripts   (#706)
    * Oracle WebCenter Sites Docker Scripts

---
+++ OracleWebCenterSites/dockerfiles/12.2.1.3/sites-container-scripts/replaceSitesTokens.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#
+# Description: Every time on run of Managed Container a new IP address might get generated. So there is a need for replacement of managedserver IP address in jbossTicketCacheReplicationConfig.xml
+#
+replaceWith=$DOCKER_HOST
+replaceString=$WCSITES_ADMIN_HOSTNAME
+
+location="/u01/oracle/user_projects/domains/base_domain/wcsites/wcsites/config/"
+
+echo The following list of files are found in location ${location} that contains ${replaceString}, will be replaced with ${replaceWith}
+#grep -rl ${replaceString} ${location} | xargs sed -i "s/${replaceString}/${replaceWith}/g"
+grep -rl --exclude="jbossTicketCacheReplicationConfig.xml" ${replaceString} ${location}
+grep -rl --exclude="jbossTicketCacheReplicationConfig.xml" ${replaceString} ${location} | xargs sed -i "s/${replaceString}/${replaceWith}/g"
---
+++ OracleWebCenterSites/dockerfiles/12.2.1.3/wcs-wls-docker-install/config.py
@@ -0,0 +1,142 @@
+# Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+#
+import os
+import sys
+
+import com.oracle.cie.domain.script.jython.WLSTException as WLSTException
+
+
+selectTemplate('Basic WebLogic Server Domain')
+loadTemplates()
+cd('/Security/base_domain/User/<WL_USERNAME>')
+cmo.setPassword('<WL_PASSWORD>') 
+cd('/Server/AdminServer')
+cmo.setName('AdminServer')
+cmo.setListenPort(<ADMIN_SERVER_PORT>)
+cmo.setListenAddress('<SERVER_HOST>')
+create('AdminServer','SSL')
+cd('SSL/AdminServer')
+cmo.setEnabled(true)
+cmo.setListenPort(<ADMIN_SERVER_SSL_PORT>)
+cmo.setHostnameVerificationIgnored(true)
+cmo.setHostnameVerifier(None)
+cmo.setTwoWaySSLEnabled(false)
+writeDomain('<DOMAIN_HOME>')
+closeTemplate()
+
+# configure Enterprise Manager
+readDomain('<DOMAIN_HOME>')
+selectTemplate('Oracle Enterprise Manager')
+loadTemplates() 
+
+dbase="<DATABASE>"
+# opss data sources
+cd('/JdbcSystemResource/opss-data-source/JdbcResource/opss-data-source/JdbcDriverParams/NO_NAME')
+cmo.setUrl('<DB_URL>')
+cmo.setDriverName('<DB_DRIVER>')
+set('PasswordEncrypted', '<RCU_SCHEMA_PASSWORD>')
+cd('Properties/NO_NAME/Property/user')
+cmo.setValue('<RCU_SCHEMA_PREFIX>_OPSS')
+if dbase!="ORACLE":
+	cd('../..')
+	create('serverName','Property')
+	cd('Property/serverName')
+	cmo.setValue('<DB_HOST>')
+	cd('../..')
+	create('portNumber','Property')
+	cd('Property/portNumber')
+	cmo.setValue('<DB_PORT>')
+	cd('../..')
+	create('databaseName','Property')
+	cd('Property/databaseName')
+	cmo.setValue('<DB_SID>')
+
+cd('/JdbcSystemResource/opss-audit-DBDS/JdbcResource/opss-audit-DBDS/JdbcDriverParams/NO_NAME')
+cmo.setUrl('<DB_URL>')
+cmo.setDriverName('<DB_DRIVER>')
+set('PasswordEncrypted', '<RCU_SCHEMA_PASSWORD>')
+cd('Properties/NO_NAME/Property/user')
+cmo.setValue('<RCU_SCHEMA_PREFIX>_IAU_APPEND')
+if dbase!="ORACLE":
+	cd('../..')
+	create('serverName','Property')
+	cd('Property/serverName')
+	cmo.setValue('<DB_HOST>')
+	cd('../..')
+	create('portNumber','Property')
+	cd('Property/portNumber')
+	cmo.setValue('<DB_PORT>')
+	cd('../..')
+	create('databaseName','Property')
+	cd('Property/databaseName')
+	cmo.setValue('<DB_SID>')	
+	
+cd('/JdbcSystemResource/opss-audit-viewDS/JdbcResource/opss-audit-viewDS/JdbcDriverParams/NO_NAME')
+
+cmo.setUrl('<DB_URL>')
+cmo.setDriverName('<DB_DRIVER>')
+set('PasswordEncrypted', '<RCU_SCHEMA_PASSWORD>')
+cd('Properties/NO_NAME/Property/user')
+cmo.setValue('<RCU_SCHEMA_PREFIX>_IAU_VIEWER')
+if dbase!="ORACLE":
+	cd('../..')
+	create('serverName','Property')
+	cd('Property/serverName')
+	cmo.setValue('<DB_HOST>')
+	cd('../..')
+	create('portNumber','Property')
+	cd('Property/portNumber')
+	cmo.setValue('<DB_PORT>')
+	cd('../..')
+	create('databaseName','Property')
+	cd('Property/databaseName')
+	cmo.setValue('<DB_SID>')	
+
+cd('/JdbcSystemResource/LocalSvcTblDataSource/JdbcResource/LocalSvcTblDataSource/JdbcDriverParams/NO_NAME')
+cmo.setUrl('<DB_URL>')
+cmo.setDriverName('<DB_DRIVER>')
+set('PasswordEncrypted', '<RCU_SCHEMA_PASSWORD>')
+cd('Properties/NO_NAME/Property/user')
+cmo.setValue('<RCU_SCHEMA_PREFIX>_STB')
+if dbase!="ORACLE":
+	cd('../..')
+	create('serverName','Property')
+	cd('Property/serverName')
+	cmo.setValue('<DB_HOST>')
+	cd('../..')
+	create('portNumber','Property')
+	cd('Property/portNumber')
+	cmo.setValue('<DB_PORT>')
+	cd('../..')
+	create('databaseName','Property')
+	cd('Property/databaseName')
+	cmo.setValue('<DB_SID>')	
+	
+updateDomain()
+closeDomain()
+
+# configure webcenter sites
+readDomain('<DOMAIN_HOME>')
+selectTemplate('<WCSITES_TEMPLATE_TYPE>') 
+loadTemplates()
+# change to sites server name
+cd('/Server/<SITES_SERVER_NAME>')
+set('Name','<SITES_SERVER_NAME>')
+cmo.setListenPort(<SITES_SERVER_PORT>)
+create('<SITES_SERVER_NAME>','SSL')
+cd('SSL/<SITES_SERVER_NAME>')
+cmo.setEnabled(true)
+cmo.setListenPort(<SITES_SERVER_SSL_PORT>)
+cd('/JdbcSystemResource/<SITES_DATASOURCE>/JdbcResource/<SITES_DATASOURCE>/JdbcDriverParams/NO_NAME')
+cmo.setUrl('<DB_URL>')
+cmo.setDriverName('<DB_DRIVER>')
+set('PasswordEncrypted', '<RCU_SCHEMA_PASSWORD>')
+cd('Properties/NO_NAME/Property/user')
+cmo.setValue('<RCU_SCHEMA_PREFIX>_WCSITES')
+
+updateDomain()
+closeDomain()
+
+exit()
