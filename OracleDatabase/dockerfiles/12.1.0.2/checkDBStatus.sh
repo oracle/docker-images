a55704b6a638be7b33d78870b9fb65de0d39af11
Gerald Venzl <gerald.venzl@gmail.com>
2018-01-24 18:10:34 +0200

    Healthcheck based on PDB

--- OracleDatabase/dockerfiles/12.1.0.2/checkDBStatus.sh
+++ OracleDatabase/dockerfiles/12.1.0.2/checkDBStatus.sh
@@ -6,14 +6,15 @@
 # Since: May, 2017
 # Author: gerald.venzl@oracle.com
 # Description: Checks the status of Oracle Database.
-# Return codes: 0 = Database is open and ready to use
-#               1 = Database is not open
+# Return codes: 0 = PDB is open and ready to use
+#               1 = PDB is not open
 #               2 = Sql Plus execution failed
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 # 
 
 ORACLE_SID="`grep $ORACLE_HOME /etc/oratab | cut -d: -f1`"
 ORACLE_PDB="`ls -dl $ORACLE_BASE/oradata/$ORACLE_SID/*/ | grep -v pdbseed | awk '{print $9}' | cut -d/ -f6`"
+POSITIVE_RETURN="READ WRITE"
 ORAENV_ASK=NO
 source oraenv
 
@@ -21,18 +22,18 @@ source oraenv
 status=`sqlplus -s / as sysdba << EOF
    set heading off;
    set pagesize 0;
-   select status from v\\$instance;
+   SELECT open_mode FROM v\\$pdbs WHERE name = '$ORACLE_PDB';
    exit;
 EOF`
 
 # Store return code from SQL*Plus
 ret=$?
 
-# SQL Plus execution was successful and database is open
-if [ $ret -eq 0 ] && [ "$status" = "OPEN" ]; then
+# SQL Plus execution was successful and PDB is open
+if [ $ret -eq 0 ] && [ "$status" = "$POSITIVE_RETURN" ]; then
    exit 0;
-# Database is not open
-elif [ "$status" != "OPEN" ]; then
+# PDB is not open
+elif [ "$status" != "$POSITIVE_RETURN" ]; then
    exit 1;
 # SQL Plus execution failed
 else
