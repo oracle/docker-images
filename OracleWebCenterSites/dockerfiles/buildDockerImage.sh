742468a22bfaf5c10dbafceacd37434d1121028e
Ashish Chalak <ashish.chalak@oracle.com>
2018-02-16 09:09:03 +0000

    Update buildDockerImage.sh
    removing comment

--- OracleWebCenterSites/dockerfiles/buildDockerImage.sh
+++ OracleWebCenterSites/dockerfiles/buildDockerImage.sh
@@ -70,7 +70,7 @@ done
 # Creating wcs-wls-docker-install.jar
 cd $VERSION/wcs-wls-docker-install
 docker run --rm -it -u root -v ${PWD}:/wcs-wls-docker-install groovy:jdk /wcs-wls-docker-install/packagejar.sh
-#docker rmi groovy:jdk
+docker rmi groovy:jdk
 cd ..
 
 # WebCenterSites Image Name
