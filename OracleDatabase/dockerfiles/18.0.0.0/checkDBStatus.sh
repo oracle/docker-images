2dd4a3318dde40f8a1f6764f4e092720ae29843e
Marcelo Ochoa <mochoa@temperies.com>
2018-03-24 17:23:59 -0300

    Docker scripts files for building 18.0.0.0 RDBMS image

---
+++ OracleDatabase/dockerfiles/18.0.0.0/checkDBStatus.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# LICENSE UPL 1.0
+#
+# Copyright (c) 1982-2017 Oracle and/or its affiliates. All rights reserved.
+#
+# Since: May, 2017
+# Author: gerald.venzl@oracle.com
+# Description: Checks the status of Oracle Database.
+# Return codes: 0 = Database is open and ready to use
+#               1 = Database is not open
+#               2 = Sql Plus execution failed
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
+
+ORACLE_SID="`grep $ORACLE_HOME /etc/oratab | cut -d: -f1`"
+ORACLE_PDB="`ls -dl $ORACLE_BASE/oradata/$ORACLE_SID/*/ | grep -v pdbseed | awk '{print $9}' | cut -d/ -f6`"
+ORAENV_ASK=NO
+source oraenv
+
+# Check Oracle DB status and store it in status
+status=`sqlplus -s / as sysdba << EOF
+   set heading off;
+   set pagesize 0;
+   select status from v\\$instance;
+   exit;
+EOF`
+
+# Store return code from SQL*Plus
+ret=$?
+
+# SQL Plus execution was successful and database is open
+if [ $ret -eq 0 ] && [ "$status" = "OPEN" ]; then
+   exit 0;
+# Database is not open
+elif [ "$status" != "OPEN" ]; then
+   exit 1;
+# SQL Plus execution failed
+else
+   exit 2;
+fi;
---
+++ OracleDatabase/dockerfiles/18.0.0.0/exadata_wrap.c
@@ -0,0 +1,4 @@
+long __wrap_ksz_exadata_feature_on(){
+    return 1;
+}
+
