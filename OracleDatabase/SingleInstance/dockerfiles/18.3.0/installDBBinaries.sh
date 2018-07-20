e6567f95e59f61dcc7ef3bcaa6e4b4bde1f11e1f
Gerald Venzl <gerald.venzl@gmail.com>
2018-07-20 09:46:23 -0700

    18c Docker build files

---
+++ OracleDatabase/SingleInstance/dockerfiles/18.3.0/installDBBinaries.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# LICENSE UPL 1.0
+#
+# Copyright (c) 1982-2016 Oracle and/or its affiliates. All rights reserved.
+#
+# Since: December, 2016
+# Author: gerald.venzl@oracle.com
+# Description: Sets up the unix environment for DB installation.
+# 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
+
+EDITION=$1
+
+# Check whether edition has been passed on
+if [ "$EDITION" == "" ]; then
+   echo "ERROR: No edition has been passed on!"
+   echo "Please specify the correct edition!"
+   exit 1;
+fi;
+
+# Check whether correct edition has been passed on
+if [ "$EDITION" != "EE" -a "$EDITION" != "SE2" ]; then
+   echo "ERROR: Wrong edition has been passed on!"
+   echo "Edition $EDITION is no a valid edition!"
+   exit 1;
+fi;
+
+# Check whether ORACLE_BASE is set
+if [ "$ORACLE_BASE" == "" ]; then
+   echo "ERROR: ORACLE_BASE has not been set!"
+   echo "You have to have the ORACLE_BASE environment variable set to a valid value!"
+   exit 1;
+fi;
+
+# Check whether ORACLE_HOME is set
+if [ "$ORACLE_HOME" == "" ]; then
+   echo "ERROR: ORACLE_HOME has not been set!"
+   echo "You have to have the ORACLE_HOME environment variable set to a valid value!"
+   exit 1;
+fi;
+
+
+# Replace place holders
+# ---------------------
+sed -i -e "s|###ORACLE_EDITION###|$EDITION|g" $INSTALL_DIR/$INSTALL_RSP && \
+sed -i -e "s|###ORACLE_BASE###|$ORACLE_BASE|g" $INSTALL_DIR/$INSTALL_RSP && \
+sed -i -e "s|###ORACLE_HOME###|$ORACLE_HOME|g" $INSTALL_DIR/$INSTALL_RSP
+
+# Install Oracle binaries
+cd $ORACLE_HOME       && \
+mv $INSTALL_DIR/$INSTALL_FILE_1 $ORACLE_HOME/ && \
+unzip $INSTALL_FILE_1 && \
+rm $INSTALL_FILE_1    && \
+$ORACLE_HOME/runInstaller -silent -force -waitforcompletion -responsefile $INSTALL_DIR/$INSTALL_RSP -ignorePrereqFailure && \
+cd $HOME
+
+# Remove not needed components
+rm -rf $ORACLE_HOME/apex && \
+rm -rf $ORACLE_HOME/jdbc && \
+# ZDLRA installer files
+rm -rf $ORACLE_HOME/lib/ra*.zip && \
+rm -rf $ORACLE_HOME/ords && \
+rm -rf $ORACLE_HOME/sqldeveloper && \
+rm -rf $ORACLE_HOME/ucp && \
+# OUI backup
+rm -rf $ORACLE_HOME/inventory/backup/* && \
+# Network tools help
+rm -rf $ORACLE_HOME/network/tools/help/mgr/help_* && \
+# Temp location
+rm -rf /tmp/*
+
+# Link password reset file to home directory
+ln -s $ORACLE_BASE/$PWD_FILE $HOME/
---
+++ OracleDatabase/SingleInstance/dockerfiles/18.3.0/setupLinuxEnv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# LICENSE UPL 1.0
+#
+# Copyright (c) 1982-2018 Oracle and/or its affiliates. All rights reserved.
+#
+# Since: December, 2016
+# Author: gerald.venzl@oracle.com
+# Description: Sets up the unix environment for DB installation.
+# 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
+
+# Setup filesystem and oracle user
+# Adjust file permissions, go to /opt/oracle as user 'oracle' to proceed with Oracle installation
+# ------------------------------------------------------------
+mkdir -p $ORACLE_BASE/scripts/setup && \
+mkdir $ORACLE_BASE/scripts/startup && \
+ln -s $ORACLE_BASE/scripts /docker-entrypoint-initdb.d && \
+mkdir $ORACLE_BASE/oradata && \
+mkdir -p $ORACLE_HOME && \
+chmod ug+x $ORACLE_BASE/*.sh && \
+yum -y install oracle-database-server-12cR2-preinstall unzip tar openssl && \
+rm -rf /var/cache/yum && \
+echo oracle:oracle | chpasswd && \
+chown -R oracle:dba $ORACLE_BASE
