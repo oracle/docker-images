3c9006a76bc21f7584141c41b94aae71b78e638f
Gerald Venzl <gerald.venzl@gmail.com>
2018-08-02 08:09:45 -0700

    Fix local_listener parameter
    Signed-off-by: Gerald Venzl <gerald.venzl@gmail.com>

--- OracleDatabase/SingleInstance/dockerfiles/18.3.0/createDB.sh
+++ OracleDatabase/SingleInstance/dockerfiles/18.3.0/createDB.sh
@@ -74,9 +74,10 @@ echo "$ORACLE_PDB=
   )
 )" >> $ORACLE_HOME/network/admin/tnsnames.ora
 
-# Remove second control file, make PDB auto open
+# Remove second control file, fix local_listener, make PDB auto open
 sqlplus / as sysdba << EOF
    ALTER SYSTEM SET control_files='$ORACLE_BASE/oradata/$ORACLE_SID/control01.ctl' scope=spfile;
+   ALTER SYSTEM SET local_listener='';
    ALTER PLUGGABLE DATABASE $ORACLE_PDB SAVE STATE;
    exit;
 EOF
