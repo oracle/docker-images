9dc3ac30b1197fc5ee0e02f268bf6173360dc15a
Gerald Venzl <gerald.venzl@gmail.com>
2018-09-11 12:51:54 -0700

    Move unzip to install image, move symlink, remove cleanup as not needed
    Signed-off-by: Gerald Venzl <gerald.venzl@gmail.com>

--- OracleDatabase/SingleInstance/dockerfiles/18.3.0/setupLinuxEnv.sh
+++ OracleDatabase/SingleInstance/dockerfiles/18.3.0/setupLinuxEnv.sh
@@ -18,9 +18,9 @@ mkdir $ORACLE_BASE/scripts/startup && \
 ln -s $ORACLE_BASE/scripts /docker-entrypoint-initdb.d && \
 mkdir $ORACLE_BASE/oradata && \
 mkdir -p $ORACLE_HOME && \
-ln -s $ORACLE_BASE/$PWD_FILE $HOME/ && \
 chmod ug+x $ORACLE_BASE/*.sh && \
-yum -y install oracle-database-preinstall-18c unzip openssl && \
+yum -y install oracle-database-preinstall-18c openssl && \
 rm -rf /var/cache/yum && \
+ln -s $ORACLE_BASE/$PWD_FILE /home/oracle/ && \
 echo oracle:oracle | chpasswd && \
 chown -R oracle:dba $ORACLE_BASE
