205f05909f84b70c3300b328f4a3b7a57fcf6eb1
Gerald Venzl <gerald.venzl@gmail.com>
2018-09-13 14:26:36 -0700

    Leveraging multi-stage builds for Oracle Database Docker images (#979)
    * Include Docker version check
    * Use Multi-Stage builds
    * Consolidate Dockerfile
    * Create password reset file symlink during Linux setup
    * Uppercase edition
    * Move unzip to install image, move symlink, remove cleanup as not needed
    * Introduce separate dockerfile for 12.1.0.2
    * Remove SE2 file for 18.3.0
    * Multi-Stage build for 12.2.0.1
    * Fix if condition
    * Add pruning of dangling images
    * MS build for 12.1.0.2
    
    Signed-off-by: Gerald Venzl <gerald.venzl@gmail.com>

--- OracleDatabase/SingleInstance/dockerfiles/12.1.0.2/setupLinuxEnv.sh
+++ OracleDatabase/SingleInstance/dockerfiles/12.1.0.2/setupLinuxEnv.sh
@@ -17,8 +17,10 @@ mkdir -p $ORACLE_BASE/scripts/setup && \
 mkdir $ORACLE_BASE/scripts/startup && \
 ln -s $ORACLE_BASE/scripts /docker-entrypoint-initdb.d && \
 mkdir $ORACLE_BASE/oradata && \
+mkdir -p $ORACLE_HOME && \
 chmod ug+x $ORACLE_BASE/*.sh && \
-yum -y install oracle-rdbms-server-12cR1-preinstall unzip tar openssl && \
+yum -y install oracle-rdbms-server-12cR1-preinstall tar openssl && \
 rm -rf /var/cache/yum && \
+ln -s $ORACLE_BASE/$PWD_FILE /home/oracle/ && \
 echo oracle:oracle | chpasswd && \
 chown -R oracle:dba $ORACLE_BASE
