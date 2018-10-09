66efcd8080d5971fb3d7a26f1f102a4d7e135336
Stephen Balousek <stephen.balousek@oracle.com>
2018-10-09 04:06:14 -0700

    Oracle GoldenGate on Docker
     - Ensure generated password meets complexity requirements for OGG 19.1.0.0.0
    
    Signed-off-by: Stephen Balousek <stephen.balousek@oracle.com>

--- OracleGoldenGate/runOracleGoldenGate.sh
+++ OracleGoldenGate/runOracleGoldenGate.sh
@@ -31,11 +31,25 @@ OGGProcesses="(adminclient|adminsrvr|distsrvr|extract|ggsci|pmsrvr|recvsrvr|repl
 }
 
 ##
+## Generate a random password with:
+##  - at least one uppercase character
+##  - at least one lowercase character
+##  - at least one digit character
+##
+function generatePassword {
+    local password="$(openssl rand -base64 9)-$(openssl rand -base64 3)"
+    [[ "${password}"  == "${password/[A-Z]/_}" ]] && { generatePassword; return }
+    [[ "${password}"  == "${password/[a-z]/_}" ]] && { generatePassword; return }
+    [[ "${password}"  == "${password/[0-9]/_}" ]] && { generatePassword; return }
+    export OGG_ADMIN_PWD="${password}"
+}
+
+##
 ## Set up administrator password for Microservices Architecture
 ##
 if [[ "${OGG_EDITION}" == "microservices" ]]; then
     if [[ -z "${OGG_ADMIN_PWD}" ]]; then
-         export OGG_ADMIN_PWD="$(openssl rand -base64 9)"
+         generatePassword
          echo "----------------------------------------------------------------------------------"
          echo "--  Password for administrative user '${OGG_ADMIN}' is '${OGG_ADMIN_PWD}'"
          echo "----------------------------------------------------------------------------------"
