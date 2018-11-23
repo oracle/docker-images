36c1567d4d5af7f09df0ce88184ceef7d2e9a455
Marcelo Ochoa <mochoa@temperies.com>
2018-11-22 23:23:34 -0300

    added missing ] in if check

--- OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh
+++ OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh
@@ -138,7 +138,7 @@ elif [ $EXPRESS -eq 1 ]; then
 fi;
 
 # Which Dockerfile should be used?
-if [ "$VERSION" == "12.1.0.2" ] || [ "$VERSION" == "11.2.0.2" || [ "$VERSION" == "18.4.0" ]; then
+if [ "$VERSION" == "12.1.0.2" ] || [ "$VERSION" == "11.2.0.2" ] || [ "$VERSION" == "18.4.0" ]; then
   DOCKERFILE="$DOCKERFILE.$EDITION"
 fi;
 
