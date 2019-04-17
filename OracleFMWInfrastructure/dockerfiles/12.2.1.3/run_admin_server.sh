3e395292af10897eb4a508888497142b65990421
Monica Riccelli <monica.riccelli@oracle.com>
2019-04-17 16:58:14 -0400

    Set the domain host volume via the domain.proprties file

--- OracleFMWInfrastructure/dockerfiles/12.2.1.3/run_admin_server.sh
+++ OracleFMWInfrastructure/dockerfiles/12.2.1.3/run_admin_server.sh
@@ -4,7 +4,6 @@
 #
 #Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 
-
 set_context() {
    scriptDir="$( cd "$( dirname "$0" )" && pwd )"
    if [ ! -d "${scriptDir}" ]; then
@@ -19,19 +18,21 @@ set_context
 . ${scriptDir}/container-scripts/setEnv.sh ${scriptDir}/properties/domain.properties
 
 # HOST volume where the domain home will be persisted
-hostvolume=/home/username/domain_home
-echo "Host volume to write the domain is: $hostvolume"
+domain_host_volume() {
+
+   domainhostvol=${DOMAIN_HOST_VOLUME:-"/home/username/domain_home"}
+   echo "Host volume to write the domain is: $domainhostvol"
+}
 
 admin_host() {
    adminhost=${ADMIN_HOST:-"InfraAdminContainer"}
+   echo "Admin Host is: $adminhost"
 }
 
-echo "Admin Host is: ${ADMIN_HOST}" 
-echo "ENV_ARG is: ${ENV_ARG}"
-
 admin_host
+domain_host_volume
 
-echo " docker run -d -p 9001:7001 -p 9002:9002 --name ${adminhost} --network=InfraNET -v ${scriptDir}/properties:/u01/oracle/properties -v ${hostvolume}:/u01/oracle/user_projects/domains ${ENV_ARG} oracle/fmw-infrastructure:12.2.1.3"
+echo " docker run -d -p 9001:7001 -p 9002:9002 --name ${adminhost} --network=InfraNET -v ${scriptDir}/properties:/u01/oracle/properties -v ${domainhostvol}:/u01/oracle/user_projects/domains ${ENV_ARG} oracle/fmw-infrastructure:12.2.1.3"
 
-docker run -d -p 9001:7001 -p 9002:9002 --name ${adminhost} --network=InfraNET -v ${scriptDir}/properties:/u01/oracle/properties -v ${hostvolume}:/u01/oracle/user_projects/domains ${ENV_ARG} oracle/fmw-infrastructure:12.2.1.3
+docker run -d -p 9001:7001 -p 9002:9002 --name ${adminhost} --network=InfraNET -v ${scriptDir}/properties:/u01/oracle/properties -v ${domainhostvol}:/u01/oracle/user_projects/domains ${ENV_ARG} oracle/fmw-infrastructure:12.2.1.3
 
--- OracleFMWInfrastructure/dockerfiles/12.2.1.3/run_managed_server.sh
+++ OracleFMWInfrastructure/dockerfiles/12.2.1.3/run_managed_server.sh
@@ -19,22 +19,18 @@ set_context() {
 set_context
 . ${scriptDir}/container-scripts/setEnv.sh ${scriptDir}/properties/domain.properties
 
-hostvolume=$HOST_VOLUME
-
 admin_host() {
    adminhost=${ADMIN_HOST:-"InfraAdminContainer"}
+   echo "adminhost= ${adminhost}"
 }
 
 managed_name() {
    managedname=${MANAGED_NAME:-"infraServer1"}
-   echo ${managed_name}
+   echo "managedname= ${managedname}"
 }
-
 
 admin_host
 managed_name
-echo "Admin Host is: ${adminhost}"
-echo "ENV_ARG is: ${ENV_ARG}"
 
 echo "docker run -d -p 9802:8002 --network=InfraNET -v ${scriptDir}/properties:/u01/oracle/properties ${ENV_ARG} --volumes-from ${adminhost} --name ${managedname}  oracle/fmw-infrastructure:12.2.1.3 startManagedServer.sh"
 
