375d72d362fbd0189473fa8a70f308e0a2c5d22b
Ashish Chalak <ashish.chalak@oracle.com>
2019-07-15 16:52:16 +0530

    Update config.py in standard with WLS/Infra

--- OracleWebCenterSites/dockerfiles/12.2.1.3/wcs-wls-docker-install/config.py
+++ OracleWebCenterSites/dockerfiles/12.2.1.3/wcs-wls-docker-install/config.py
@@ -1,17 +1,12 @@
-#
-#
-# Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
-#
-# Licensed under the Universal Permissive License v 1.0 as shown at
-# http://oss.oracle.com/licenses/upl.
-#
+# Copyright 2019, Oracle Corporation and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 
 import os
 import sys
 
 import com.oracle.cie.domain.script.jython.WLSTException as WLSTException
 
-class WCSITES12212Provisioner:
+class WCSITES12213Provisioner:
 
     MACHINES = {
         '<MACHINE_NAME>' : {
@@ -33,16 +28,9 @@ class WCSITES12212Provisioner:
         }
     }
 
-    WCSITES_SERVERS = {
-        '<SITES_SERVER_NAME>' : {
-            'ListenAddress': '',
-            'ListenPort': <SITES_SERVER_PORT>,
-            'Machine': '<MACHINE_NAME>',
-            'Cluster': '<CLUSTER_NAME>'
-        }
-    }
-
-    JRF_12212_TEMPLATES = {
+    MANAGED_SERVERS = []
+    
+    JRF_12213_TEMPLATES = {
         'baseTemplate' : '@@ORACLE_HOME@@/wlserver/common/templates/wls/wls.jar',
         'extensionTemplates' : [
             '@@ORACLE_HOME@@/oracle_common/common/templates/wls/oracle.jrf_template.jar',
@@ -51,39 +39,65 @@ class WCSITES12212Provisioner:
         'serverGroupsToTarget' : [ 'JRF-MAN-SVR' ]
     }
 
-    WCSITES_12212_TEMPLATES = {
+    WCSITES_12213_TEMPLATES = {
         'extensionTemplates' : [
             '@@ORACLE_HOME@@/wcsites/common/templates/wls/oracle.wcsites.examples.template.jar'
         ],
         'serverGroupsToTarget' : [ 'WCSITES-MGD-SVR' ]
     }
 
-    def __init__(self, oracleHome, javaHome, domainParentDir):
+    def __init__(self, oracleHome, javaHome, domainParentDir, adminListenPort, adminName, managedNameBase, managedServerPort, prodMode, managedCount, clusterName):
         self.oracleHome = self.validateDirectory(oracleHome)
         self.javaHome = self.validateDirectory(javaHome)
         self.domainParentDir = self.validateDirectory(domainParentDir, create=True)
         return
 
-    def createDomain(self, name, user, password, db, dbPrefix, dbPassword,domainType):
-        domainHome = self.createBaseDomain(name, user, password,domainType)
-
+    def createInfraDomain(self, domainName, user, password, db, dbPrefix, dbPassword, adminListenPort, adminName, managedNameBase, managedServerPort, prodMode, managedCount, clusterName, domainType,
+                          exposeAdminT3Channel=None, t3ChannelPublicAddress=None, t3ChannelPort=None):
+        domainHome = self.createBaseDomain(domainName, user, password, adminListenPort, adminName, managedNameBase, managedServerPort, prodMode, managedCount, clusterName, domainType)
+        
         if domainType == "wcsites":
-                self.extendWcsitesDomain(domainHome, db, dbPrefix, dbPassword)
+                self.extendWcsitesDomain(domainHome, db, dbPrefix, dbPassword, exposeAdminT3Channel, t3ChannelPublicAddress, t3ChannelPort)
 
-    def createBaseDomain(self, name, user, password,domainType):
-        baseTemplate = self.replaceTokens(self.JRF_12212_TEMPLATES['baseTemplate'])
-
+    def createBaseDomain(self, domainName, user, password, adminListenPort, adminName, managedNameBase, managedServerPort, prodMode, managedCount, clusterName, domainType):
+        baseTemplate = self.replaceTokens(self.JRF_12213_TEMPLATES['baseTemplate'])
+        
         readTemplate(baseTemplate)
-        setOption('DomainName', name)
+        setOption('DomainName', domainName)
         setOption('JavaHome', self.javaHome)
-        setOption('ServerStartMode', 'prod')
+        if (prodMode == 'true'):
+            setOption('ServerStartMode', 'prod')
+        else:
+            setOption('ServerStartMode', 'dev')
         set('Name', domainName)
+        
+        admin_port = int(adminListenPort)
+        ms_port    = int(managedServerPort)
+        ms_count   = int(managedCount)
+        
+        # Create Admin Server
+        # =======================
+        print 'Creating Admin Server...'
+        cd('/Servers/AdminServer')
+        #set('ListenAddress', '%s-%s' % (domain_uid, admin_server_name_svc))
+        set('ListenPort', admin_port)
+        set('Name', adminName)
+		
+		# Define the user password for weblogic
+        # =====================================
         cd('/Security/' + domainName + '/User/weblogic')
         set('Name', user)
         set('Password', password)
-
-
-        print 'INFO: Creating Node Managers...'
+        
+        # Create a cluster
+        # ======================
+        print 'Creating cluster...'
+        cd('/')
+        cl=create(clusterName, 'Cluster')
+		
+		# Create Node Manager
+        # =======================
+        print 'Creating Node Managers...'
         for machine in self.MACHINES:
             cd('/')
             create(machine, 'Machine')
@@ -92,69 +106,64 @@ class WCSITES12212Provisioner:
             cd('NodeManager/' + machine)
             for param in self.MACHINES[machine]:
                 set(param, self.MACHINES[machine][param])
-
-        print 'INFO: Creating Admin server...'
-
-        for server in self.SERVERS:
+        # Create managed servers
+        for index in range(0, ms_count):
             cd('/')
-            if server == 'AdminServer':
-                cd('Server/' + server)
-                for param in self.SERVERS[server]:
-                        set(param, self.SERVERS[server][param])
-                continue
-            create(server, 'Server')
-            cd('Server/' + server)
-            for param in self.SERVERS[server]:
-                set(param, self.SERVERS[server][param])
-
-        if domainType == "wcsites":
-                for cluster in self.WCSITES_CLUSTERS:
-                        cd('/')
-                        create(cluster, 'Cluster')
-                        cd('Cluster/' + cluster)
-                        for param in  self.WCSITES_CLUSTERS[cluster]:
-                                set(param, self.WCSITES_CLUSTERS[cluster][param])
-
-                for server in self.WCSITES_SERVERS:
-                        cd('/')
-                        create(server, 'Server')
-                        cd('Server/' + server)
-                        for param in self.WCSITES_SERVERS[server]:
-                                set(param, self.WCSITES_SERVERS[server][param])
-
-                print 'INFO: WCSITES Servers created.....'
-
+            msIndex = index+1
+            cd('/')
+            name = '%s%s' % (managedNameBase, msIndex)
+            create(name, 'Server')
+            cd('/Servers/%s/' % name )
+            print('managed server name is %s' % name);
+            set('ListenPort', ms_port)
+            set('NumOfRetriesBeforeMSIMode', 0)
+            set('RetryIntervalBeforeMSIMode', 1)
+            set('Cluster', clusterName)
+            set('Machine', machineName)
+            self.MANAGED_SERVERS.append(name)
+        print self.MANAGED_SERVERS
+        
         setOption('OverwriteDomain', 'true')
-        domainHome = self.domainParentDir + '/' + name
-
-        print 'INFO: Writing base domain...'
+        domainHome = self.domainParentDir + '/' + domainName
+        print 'Will create Base domain at ' + domainHome
+        
+        print 'Writing base domain...'
         writeDomain(domainHome)
         closeTemplate()
-        print 'INFO: Base domain created at ' + domainHome
+        print 'Base domain created at ' + domainHome
         return domainHome
 
-
-    def readAndApplyJRFTemplates(self, domainHome):
-        print 'INFO: Extending domain at ' + domainHome
+    def readAndApplyJRFTemplates(self, domainHome, db, dbPrefix, dbPassword, exposeAdminT3Channel, t3ChannelPublicAddress, t3ChannelPort):
+        print 'Extending domain at ' + domainHome
+        print 'Database  ' + db
         readDomain(domainHome)
         setOption('AppDir', self.domainParentDir + '/applications')
+        
+        print 'ExposeAdminT3Channel %s with %s:%s ' % (exposeAdminT3Channel, t3ChannelPublicAddress, t3ChannelPort)
+        if 'true' == exposeAdminT3Channel:
+            self.enable_admin_channel(t3ChannelPublicAddress, t3ChannelPort)
+        
+        self.applyJRFTemplates()
+        print 'Extension Templates added'
+        return
 
-        print 'INFO: Applying JRF templates...'
-        for extensionTemplate in self.JRF_12212_TEMPLATES['extensionTemplates']:
+    def applyJRFTemplates(self):
+        print 'Applying JRF templates...'
+        for extensionTemplate in self.JRF_12213_TEMPLATES['extensionTemplates']:
             addTemplate(self.replaceTokens(extensionTemplate))
         return
 
     def applyWCSITESTemplates(self):
-        print 'INFO: Applying WCSITES templates...'
-        for extensionTemplate in self.WCSITES_12212_TEMPLATES['extensionTemplates']:
+        print 'Applying WCSITES templates...'
+        for extensionTemplate in self.WCSITES_12213_TEMPLATES['extensionTemplates']:
             addTemplate(self.replaceTokens(extensionTemplate))
         return
 
     def configureJDBCTemplates(self,db,dbPrefix,dbPassword):
-        print 'INFO: Configuring the Service Table DataSource...'
+        print 'Configuring the Service Table DataSource...'
         fmwDb = db
         driverName = 'oracle.jdbc.OracleDriver'
-        print "INFO: fmwDb..." + fmwDb
+        print "fmwDb..." + fmwDb
         
         cd('/JdbcSystemResource/opss-data-source/JdbcResource/opss-data-source/JdbcDriverParams/NO_NAME_0')
         set('DriverName', driverName)
@@ -165,7 +174,7 @@ class WCSITES12212Provisioner:
         cd('Properties/NO_NAME_0/Property/user')
         set('Value', user)
         
-        print "INFO: Set user..." + user
+        print "Set user..." + user
         
         cd('/JdbcSystemResource/opss-audit-DBDS/JdbcResource/opss-audit-DBDS/JdbcDriverParams/NO_NAME_0')
         set('DriverName', driverName)
@@ -176,7 +185,7 @@ class WCSITES12212Provisioner:
         cd('Properties/NO_NAME_0/Property/user')
         set('Value', user)
         
-        print "INFO: Set user..." + user
+        print "Set user..." + user
         
         cd('/JdbcSystemResource/opss-audit-viewDS/JdbcResource/opss-audit-viewDS/JdbcDriverParams/NO_NAME_0')
         set('DriverName', driverName)
@@ -187,7 +196,7 @@ class WCSITES12212Provisioner:
         cd('Properties/NO_NAME_0/Property/user')
         set('Value', user)
         
-        print "INFO: Set user..." + user
+        print "Set user..." + user
         
         cd('/JdbcSystemResource/LocalSvcTblDataSource/JdbcResource/LocalSvcTblDataSource/JdbcDriverParams/NO_NAME_0')
         set('DriverName', driverName)
@@ -198,7 +207,7 @@ class WCSITES12212Provisioner:
         cd('Properties/NO_NAME_0/Property/user')
         set('Value', user)
         
-        print "INFO: Set user..." + user
+        print "Set user..." + user
         
         cd('/JdbcSystemResource/wcsitesDS/JdbcResource/wcsitesDS/JdbcDriverParams/NO_NAME_0')
         set('DriverName', driverName)
@@ -209,58 +218,58 @@ class WCSITES12212Provisioner:
         cd('Properties/NO_NAME_0/Property/user')
         set('Value', user)
         
-        print "INFO: Set user..." + user
+        print "Set user..." + user
 
-        print 'INFO: Getting Database Defaults...'
+        print 'Getting Database Defaults...'
         getDatabaseDefaults()
         return
 
     def targetWCSITESServers(self,serverGroupsToTarget):
-        for server in self.WCSITES_SERVERS:
-            if not server == 'AdminServer':
-                setServerGroups(server, serverGroupsToTarget)
-                print "INFO: Set CoherenceClusterSystemResource to defaultCoherenceCluster for server:" + server
-                cd('/Servers/' + server)
-                set('CoherenceClusterSystemResource', 'defaultCoherenceCluster')
+        print 'Targeting Server Groups...'
+        cd('/')
+        for managedName in self.MANAGED_SERVERS:
+            setServerGroups(managedName, serverGroupsToTarget)
+            print "Set CoherenceClusterSystemResource to defaultCoherenceCluster for server:" + managedName
+            cd('/Servers/' + managedName)
+            set('CoherenceClusterSystemResource', 'defaultCoherenceCluster')
         return
 
     def targetWCSITESCluster(self):
+        print 'Targeting Cluster ...'
+        cd('/')
         for cluster in self.WCSITES_CLUSTERS:
-            print "INFO: Set CoherenceClusterSystemResource to defaultCoherenceCluster for cluster:" + cluster
+            print "Set CoherenceClusterSystemResource to defaultCoherenceCluster for cluster:" + cluster
             cd('/Cluster/' + cluster)
             set('CoherenceClusterSystemResource', 'defaultCoherenceCluster')
         return
 
-    def extendWcsitesDomain(self, domainHome, db, dbPrefix, dbPassword):
-        self.readAndApplyJRFTemplates(domainHome)
+    def extendWcsitesDomain(self, domainHome, db, dbPrefix, dbPassword, exposeAdminT3Channel, t3ChannelPublicAddress, t3ChannelPort):
+        self.readAndApplyJRFTemplates(domainHome, db, dbPrefix, dbPassword, exposeAdminT3Channel, t3ChannelPublicAddress, t3ChannelPort)
         self.applyWCSITESTemplates()
-
-        print 'INFO: Extension Templates added'
-
+        
+        print 'Extension Templates added'
+        
         self.configureJDBCTemplates(db,dbPrefix,dbPassword)
-
-        print 'INFO: Targeting Server Groups...'
-        serverGroupsToTarget = list(self.JRF_12212_TEMPLATES['serverGroupsToTarget'])
-        serverGroupsToTarget.extend(self.WCSITES_12212_TEMPLATES['serverGroupsToTarget'])
-
+        
+        print 'Targeting Server Groups...'
+        serverGroupsToTarget = list(self.JRF_12213_TEMPLATES['serverGroupsToTarget'])
+        serverGroupsToTarget.extend(self.WCSITES_12213_TEMPLATES['serverGroupsToTarget'])
+        
         cd('/')
         self.targetWCSITESServers(serverGroupsToTarget)
-
+        
         cd('/')
         self.targetWCSITESCluster()
-
-        print "INFO: Set WLS clusters as target of defaultCoherenceCluster:[" + ",".join(self.WCSITES_CLUSTERS) + "]"
-
+        
+        print "Set WLS clusters as target of defaultCoherenceCluster:[" + ",".join(self.WCSITES_CLUSTERS) + "]"
         cd('/CoherenceClusterSystemResource/defaultCoherenceCluster')
         set('Target', ",".join(self.WCSITES_CLUSTERS))
-        print 'INFO: Preparing to update domain...'
+        
+        print 'Preparing to update domain...'
         updateDomain()
-        print 'INFO: Domain updated successfully'
+        print 'Domain updated successfully'
         closeDomain()
         return
-
-
- 
     ###########################################################################
     # Helper Methods                                                          #
     ###########################################################################
@@ -292,11 +301,22 @@ class WCSITES12212Provisioner:
             result = path.replace('@@ORACLE_HOME@@', oracleHome)
         return result
 
+    def enable_admin_channel(self, admin_channel_address, admin_channel_port):
+        if admin_channel_address == None or admin_channel_port == 'None':
+            return
+        cd('/')
+        admin_server_name = get('AdminServerName')
+        print('setting admin server t3channel for ' + admin_server_name)
+        cd('/Servers/' + admin_server_name)
+        create('T3Channel', 'NetworkAccessPoint')
+        cd('/Servers/' + admin_server_name + '/NetworkAccessPoint/T3Channel')
+        set('ListenPort', int(admin_channel_port))
+        set('PublicPort', int(admin_channel_port))
+        set('PublicAddress', admin_channel_address)	
+
 #############################
 # Entry point to the script #
 #############################
-
-print "createDomain.py called with the following inputs:"
 
 #oracleHome will be passed by command line parameter -oh.
 oracleHome = '<ORACLE_HOME>'
@@ -316,6 +336,10 @@ rcuDb = '<DB_URL>'
 rcuSchemaPrefix = '<RCU_SCHEMA_PREFIX>'
 #change rcuSchemaPassword to your wcsitesinfra schema password. Command line parameter -rcuSchemaPwd.
 rcuSchemaPassword = '<RCU_SCHEMA_PASSWORD>'
+exposeAdminT3Channel = None
+t3ChannelPort = None
+t3ChannelPublicAddress = None
+
 #change domainType to your wcsitesinfra. Command line parameter -domainType.
 domainType = 'wcsites'
 # Cluster Name
@@ -323,11 +347,17 @@ clusterName = '<CLUSTER_NAME>'
 # Machine Name
 machineName = '<MACHINE_NAME>'
 # Admin Port
-adminServerPort = <ADMIN_SERVER_PORT>
+adminListenPort = <ADMIN_SERVER_PORT>
 # Sites Port
-sitesServerPort = <SITES_SERVER_PORT>
+managedServerPort = <SITES_SERVER_PORT>
 # Sites Server Name
 sitesServerName = '<SITES_SERVER_NAME>'
+# To run in production mode set as true else false
+prodMode=true
 
-provisioner = WCSITES12212Provisioner(oracleHome, javaHome, domainParentDir)
-provisioner.createDomain(domainName, domainUser, domainPassword, rcuDb, rcuSchemaPrefix, rcuSchemaPassword,domainType)
+adminName='AdminServer'
+managedNameBase='<SITES_SERVER_NAME>'
+managedCount=1
+
+provisioner = WCSITES12213Provisioner(oracleHome, javaHome, domainParentDir, adminListenPort, adminName, managedNameBase, managedServerPort, prodMode, managedCount, clusterName)
+provisioner.createInfraDomain(domainName, domainUser, domainPassword, rcuDb, rcuSchemaPrefix, rcuSchemaPassword, adminListenPort, adminName, managedNameBase, managedServerPort, prodMode, managedCount, clusterName, domainType, exposeAdminT3Channel, t3ChannelPublicAddress, t3ChannelPort)
