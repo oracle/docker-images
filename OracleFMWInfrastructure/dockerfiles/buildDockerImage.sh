816855f3ae4e7c058d90f81786651538231c17c6
Monica Riccelli <monica.riccelli@oracle.com>
2019-09-27 17:18:28 -0400

    Dockerfiles and Scripts for WebLogic 12.2.1.4 (#1383)
    * Dockerfiles and scripts for WebLogic Server 12.2.1.4

--- OracleFMWInfrastructure/dockerfiles/buildDockerImage.sh
+++ OracleFMWInfrastructure/dockerfiles/buildDockerImage.sh
@@ -4,10 +4,9 @@
 # Author: monica.riccelli@oracle.com
 # Description: script to build a Docker image for FMW Infrastructure
 #
-# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+#Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
 #
-# Copyright (c) 2014-2018 Oracle and/or its affiliates. All rights reserved.
-#
+#Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 usage() {
 cat << EOF
@@ -24,7 +23,7 @@ Parameters:
 
 LICENSE UPL 1.0
 
-Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
 
 EOF
 exit 0
@@ -35,7 +34,7 @@ exit 0
 if [ "$#" -eq 0 ]; then usage; fi
 
 # Parameters
-VERSION="12.2.1.2"
+VERSION="12.2.1.4"
 SKIPMD5=0
 NOCACHE=true
 
