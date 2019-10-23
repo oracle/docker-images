99e66e14df6f3d00e9cc4617a2ce9b7a0af38934
Paramdeep Saini <paramdeep.saini@oracle.com>
2019-10-23 20:00:52 +0000

    Changes to run user scripts after Grid installation and some code cleanup

--- OracleDatabase/RAC/OracleRealApplicationClusters/dockerfiles/12.2.0.1/AddNode.sh
+++ OracleDatabase/RAC/OracleRealApplicationClusters/dockerfiles/12.2.0.1/AddNode.sh
@@ -937,6 +937,8 @@ runrootsh $GRID_HOME  $GRID_USER
 checkCluster
 print_message "Checking Cluster Class"
 checkClusterClass
+print_message "Running User Script for $GRID_USER user"
+su - $GRID_USER -c "$SCRIPT_DIR/$USER_SCRIPTS_FILE $SCRIPT_ROOT GRID"
 
 ###### DB Node Addition ######
 if [ "${CLUSTER_TYPE}" != 'Domain' ]; then
@@ -950,8 +952,8 @@ addDBInst
 print_message "Checking DB status"
 su - $DB_USER -c "$SCRIPT_DIR/$CHECK_DB_FILE $ORACLE_SID"
 checkDBStatus
-print_message "Running User Script"
-su - $DB_USER -c "$SCRIPT_DIR/$USER_SCRIPTS_FILE $SCRIPT_ROOT"
+print_message "Running User Script for $DB_USER user"
+su - $DB_USER -c "$SCRIPT_DIR/$USER_SCRIPTS_FILE $SCRIPT_ROOT DB"
 print_message "Setting Remote Listener"
 setremotelistener
 fi
--- OracleDatabase/RAC/OracleRealApplicationClusters/dockerfiles/19.3.0/runUserScripts.sh
+++ OracleDatabase/RAC/OracleRealApplicationClusters/dockerfiles/19.3.0/runUserScripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # LICENSE UPL 1.0
 #
-# Copyright (c) 1982-2019 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 1982-2018 Oracle and/or its affiliates. All rights reserved.
 #
 # Since: January, 2018
 # Author: sanjay.singh@oracle.com, paramdeep.saini@oracle.com
@@ -11,18 +11,18 @@
 # 
 
 SCRIPTS_ROOT="$1";
+SCRIPTS_TYPE="$2";
 
 # Check whether parameter has been passed on
 if [ -z "$SCRIPTS_ROOT" ]; then
    echo "$0: No SCRIPTS_ROOT passed on, no scripts will be run";
-
 else
 
 # Execute custom provided files (only if directory exists and has files in it)
-if [ -d "$SCRIPTS_ROOT" ] && [ -n "$(ls -A $SCRIPTS_ROOT)" ]; then
+if [ "${SCRIPTS_TYPE}" == "DB" ] && [ -d "$SCRIPTS_ROOT" ] && [ -n "$(ls -A $SCRIPTS_ROOT)" ]; then
 
   echo "";
-  echo "Executing user defined scripts"
+  echo "Executing user defined scripts for ${SCRIPTS_TYPE}"
 
   for f in $SCRIPTS_ROOT/*; do
       case "$f" in
@@ -33,7 +33,34 @@ if [ -d "$SCRIPTS_ROOT" ] && [ -n "$(ls -A $SCRIPTS_ROOT)" ]; then
       echo "";
   done
   
-  echo "DONE: Executing user defined scripts"
+  echo "DONE: Executing user defined scripts for ${SCRIPTS_TYPE}"
+  echo "";
+
+fi;
+
+fi;
+ 
+if [ -z "$SCRIPTS_ROOT" ]; then
+   echo "$0: No SCRIPTS_ROOT passed on, no scripts will be run";
+
+else
+
+# Execute custom provided files (only if directory exists and has files in it)
+if [ "${SCRIPTS_TYPE}" == "GRID" ] && [ -d "$SCRIPTS_ROOT" ] && [ -n "$(ls -A $SCRIPTS_ROOT)" ]; then
+
+  echo "";
+  echo "Executing user defined scripts for ${SCRIPTS_TYPE}"
+
+  for f in $SCRIPTS_ROOT/*; do
+      case "$f" in
+          *.sh)     echo "$0: running $f"; . "$f" ;;
+          *.sql)    echo "$0: running $f"; echo "exit" | $ORACLE_HOME/bin/sqlplus -s "/ as sysasm" @"$f"; echo ;;
+          *)        echo "$0: ignoring $f" ;;
+      esac
+      echo "";
+  done
+
+  echo "DONE: Executing user defined scripts for ${SCRIPTS_TYPE}"
   echo "";
 
 fi;
