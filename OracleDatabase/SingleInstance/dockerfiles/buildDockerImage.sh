e61ba92aa2f4293a573ccd9a04f9278fc518fbe4
Robert Reeves <r2@datical.com>
2020-02-13 15:32:33 -0600

    aligning version numbers in script with version directories

--- OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh
+++ OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh
@@ -160,7 +160,7 @@ elif [ $EXPRESS -eq 1 ]; then
 fi;
 
 # Which Dockerfile should be used?
-if [ "$VERSION" == "12.1.0.2" ] || [ "$VERSION" == "11.2.0.2" ] || [ "$VERSION" == "18.4.0" ]; then
+if [ "$VERSION" == "11.2.0.2" ] ||[ "$VERSION" == "12.1.0.2" ] ||[ "$VERSION" == "12.2.0.1" ] || [ "$VERSION" == "18.3.0" ] || [ "$VERSION" == "18.4.0" ] [ "$VERSION" == "19.3.0" ]; then
   DOCKERFILE="$DOCKERFILE.$EDITION"
 fi;
 
