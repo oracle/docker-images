d60937910087357abd515b122702acbde0c00db6
Quang Dang <quang.dang@oracle.com>
2020-03-23 23:58:47 +0000

    add ssl support

--- OracleWebLogic/samples/12213-domain-home-in-image/container-scripts/startManagedServer.sh
+++ OracleWebLogic/samples/12213-domain-home-in-image/container-scripts/startManagedServer.sh
@@ -39,11 +39,16 @@ if [ -z "${PASS}" ]; then
 fi
 
 #Set Java Options
-JAVA_OPTIONS=`awk '{print $1}' ${SEC_PROPERTIES_FILE} | grep ^JAVA_OPTIONS= | cut -d "=" -f2`
-if [ -z "${JAVA_OPTIONS}" ]; then 
-   JAVA_OPTIONS="-Dweblogic.StdoutDebugEnabled=false"
+JAVA_OPTIONS_PROP=`grep ^JAVA_OPTIONS= ${SEC_PROPERTIES_FILE} | cut -d "=" -f2-`
+if [ -z "${JAVA_OPTIONS_PROP}" ]; then 
+  JAVA_OPTIONS_PROP="-Dweblogic.StdoutDebugEnabled=false"
 fi
-export JAVA_OPTIONS=${JAVA_OPTIONS}
+# Use the Env JAVA_OPTIONS if it's been set
+if [ -z "$JAVA_OPTIONS" ]; then
+  export JAVA_OPTIONS=${JAVA_OPTIONS_PROP}
+else
+  JAVA_OPTIONS="${JAVA_OPTIONS} ${JAVA_OPTIONS_PROP}"
+fi
 echo "Java Options: ${JAVA_OPTIONS}"
 
 # Create Managed Server
@@ -53,8 +58,12 @@ echo "password=${PASS}" >> ${MS_SECURITY}/boot.properties
 ${DOMAIN_HOME}/bin/setDomainEnv.sh
 
 # Start 'ManagedServer'
+ADMIN_SERVER_URL="http://${ADMIN_HOST}:${ADMIN_PORT}"
+if [ "${SSL_ENABLED}" = "true" ]; then
+  ADMIN_SERVER_URL="https://${ADMIN_HOST}:${ADMIN_SERVER_SSL_PORT}" 
+fi
 echo "Start Managed Server"
-${DOMAIN_HOME}/bin/startManagedWebLogic.sh ${MANAGED_SERV_NAME} http://${ADMIN_HOST}:${ADMIN_PORT}
+${DOMAIN_HOME}/bin/startManagedWebLogic.sh ${MANAGED_SERV_NAME} ${ADMIN_SERVER_URL}
 
 # tail Managed Server log
 tail -f ${MS_HOME}/logs/${MANAGED_SERV_NAME}.log &
