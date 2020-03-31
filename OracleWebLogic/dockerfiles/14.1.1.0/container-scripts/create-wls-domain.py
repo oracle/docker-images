655ba6d4157230ccc08cc492c613f4eaa4200056
Monica Riccelli <monica.riccelli@oracle.com>
2020-03-31 19:37:55 -0400

    Correct copyright headers and chown -R

--- OracleWebLogic/dockerfiles/14.1.1.0/container-scripts/create-wls-domain.py
+++ OracleWebLogic/dockerfiles/14.1.1.0/container-scripts/create-wls-domain.py
@@ -1,5 +1,5 @@
-# Copyright 2020, Oracle Corporation and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+# Copyright (c) 2020 Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 # WebLogic on Docker Default Domain
 #
--- OracleWebLogic/dockerfiles/14.1.1.0/container-scripts/createAndStartEmptyDomain.sh
+++ OracleWebLogic/dockerfiles/14.1.1.0/container-scripts/createAndStartEmptyDomain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Copyright 2020, Oracle Corporation and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+# Copyright (c) 2020 Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 
 # If AdminServer.log does not exists, container is starting for 1st time
