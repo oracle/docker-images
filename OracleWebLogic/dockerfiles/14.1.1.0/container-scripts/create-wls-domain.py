98ce36963b63d3206d1217625e98d2ac13587c57
Monica Riccelli <monica.riccelli@oracle.com>
2020-04-01 12:08:42 -0400

    Fix copyright to remove Corporation

--- OracleWebLogic/dockerfiles/14.1.1.0/container-scripts/create-wls-domain.py
+++ OracleWebLogic/dockerfiles/14.1.1.0/container-scripts/create-wls-domain.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Oracle Corporation and/or its affiliates.
+# Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 # WebLogic on Docker Default Domain
