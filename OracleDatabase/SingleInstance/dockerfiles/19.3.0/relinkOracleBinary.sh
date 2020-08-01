d35957a1202693b4ee0c250ae4bdc898c7a9b282
Rishabh Gupta <rishabh.y.gupta@oracle.com>
2020-08-01 01:42:52 -0700

    modified formatting of help/error messages
    Signed-off-by: Rishabh Gupta <rishabh.y.gupta@oracle.com>

--- OracleDatabase/SingleInstance/dockerfiles/19.3.0/relinkOracleBinary.sh
+++ OracleDatabase/SingleInstance/dockerfiles/19.3.0/relinkOracleBinary.sh
@@ -26,13 +26,10 @@ fi
 if [ -d $ORACLE_BASE/oradata/$ORACLE_SID ]; then
     datafiles_edition=$(ls $ORACLE_BASE/oradata/dbconfig/$ORACLE_SID/.docker_* | rev | cut -d_ -f1 | rev)
     if [ "${ORACLE_EDITION}" != "" ] && [ "${ORACLE_EDITION,,}" != $datafiles_edition ]; then
-        echo "The datafiles being used were created with $datafiles_edition edition software home. \
-        Please pass -e ORACLE_EDITION=$datafiles_edition to the docker run cmd.";
+        echo "The datafiles being used were created with $datafiles_edition edition software home. Please pass -e ORACLE_EDITION=$datafiles_edition to the docker run cmd.";
         exit 1;
     elif [ "${ORACLE_EDITION}" == "" ] && [ "${CURRENT_EDITION,,}" != $datafiles_edition ]; then
-        echo "The current software home is of ${CURRENT_EDITION,,} edition whereas \
-        the datafiles being used were created with $datafiles_edition edition software home. \
-        Please pass -e ORACLE_EDITION=$datafiles_edition to the docker run cmd.";
+        echo "The current software home is of ${CURRENT_EDITION,,} edition whereas the datafiles being used were created with $datafiles_edition edition software home. Please pass -e ORACLE_EDITION=$datafiles_edition to the docker run cmd.";
         exit 1;
     fi
 fi
