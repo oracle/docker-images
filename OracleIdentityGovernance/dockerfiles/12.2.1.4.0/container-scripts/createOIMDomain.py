2d944ef9e5a0abae054ea17787a97fb8652469ba
Rishi Agarwal <68962689+rishiagarwal-oracle@users.noreply.github.com>
2020-10-14 17:52:04 +0530

    Update createOIMDomain.py

--- OracleIdentityGovernance/dockerfiles/12.2.1.4.0/container-scripts/createOIMDomain.py
+++ OracleIdentityGovernance/dockerfiles/12.2.1.4.0/container-scripts/createOIMDomain.py
@@ -1,9 +1,9 @@
-# # Copyright (c) 2020 Oracle and/or its affiliates.
-# #
-# # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
-# #
-# # Author: OIG Development 
-# #
+# Copyright (c) 2020 Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+# Author: OIG Development 
+#
 import os
 import sys
 import com.oracle.cie.domain.script.jython.WLSTException as WLSTException
