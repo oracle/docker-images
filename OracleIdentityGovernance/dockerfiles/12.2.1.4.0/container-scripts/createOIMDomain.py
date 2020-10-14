c4067ea6a2ecc10c72d01fd1a0d3a5240c4558a0
Rishi Agarwal <68962689+rishiagarwal-oracle@users.noreply.github.com>
2020-10-14 17:40:51 +0530

    Update createOIMDomain.py
    fix copyright header

--- OracleIdentityGovernance/dockerfiles/12.2.1.4.0/container-scripts/createOIMDomain.py
+++ OracleIdentityGovernance/dockerfiles/12.2.1.4.0/container-scripts/createOIMDomain.py
@@ -1,4 +1,4 @@
-# # Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+# # Copyright (c) 2020 Oracle and/or its affiliates.
 # #
 # # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # #
