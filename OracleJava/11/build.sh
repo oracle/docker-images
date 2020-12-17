82e7c82a9b1476c007aa39eb8286b5ac895ddcf4
Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>
2020-12-17 15:19:15 -0800

    Updated scripts to use 8-slim rather than slim-8, updated readmes with notes on how to choose which OS image to use
    Signed-off-by: Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>

--- OracleJava/11/build.sh
+++ OracleJava/11/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+# Copyright 2019,2020 Oracle and/or its affiliates
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-if test "$1" = "slim-8"
+if test "$1" = "8-slim"
 then
 	echo "Building Oracle JDK 11 on Oracle Linux 8 slim"
-	docker build --file=Dockerfile.slim-8 --tag oracle/jdk:11-oraclelinux8 .
+	docker build --file Dockerfile.8-slim --tag oracle/jdk:11-oraclelinux8 .
 else
 	echo "Building Oracle JDK 11 on Oracle Linux 7 slim"
-	docker build --tag oracle/jdk:11 .
+	docker build --tag oracle/jdk:11 --tag oracle/jdk:11-oraclelinux7 .
 fi
--- OracleJava/15/build.sh
+++ OracleJava/15/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+# Copyright 2020 Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-if test "$1" = "slim-8"
+if test "$1" = "8-slim"
 then
 	echo "Building Oracle JDK 15 on Oracle Linux 8 slim"
-	docker build --file Dockerfile.slim-8 --tag oracle/jdk:15-oraclelinux8 .
+	docker build --file Dockerfile.8-slim --tag oracle/jdk:15-oraclelinux8 .
 else
 	echo "Building Oracle JDK 15 on Oracle Linux 7 slim"
-	docker build --tag oracle/jdk:15 .
+	docker build --tag oracle/jdk:15 --tag oracle/jdk:15-oraclelinux7 .
 fi
