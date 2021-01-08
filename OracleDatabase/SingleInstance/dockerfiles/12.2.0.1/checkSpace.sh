d918f5a4c6da9b0f294afb8650d1f6f951964262
Philippe Vanhaesendonck <AmedeeBulle@users.noreply.github.com>
2021-01-08 19:21:21 +0100

    Database: improve support for Podman (#1829)
    * Database: improve support for Podman
    
    Signed-off-by: Philippe Vanhaesendonck <philippe.vanhaesendonck@oracle.com>
    
    * Rename buildDockerImage.sh to buildContainerImage.sh
    
    Signed-off-by: Philippe Vanhaesendonck <philippe.vanhaesendonck@oracle.com>
    
    * Lint buildContainerImage.sh with ShellCheck
    
    Signed-off-by: Philippe Vanhaesendonck <philippe.vanhaesendonck@oracle.com>
    
    * SingleInstance: update README/FAQ
    
    Signed-off-by: Philippe Vanhaesendonck <philippe.vanhaesendonck@oracle.com>

--- OracleDatabase/SingleInstance/dockerfiles/12.2.0.1/checkSpace.sh
+++ OracleDatabase/SingleInstance/dockerfiles/12.2.0.1/checkSpace.sh
@@ -16,7 +16,7 @@ AVAILABLE_SPACE_GB=`df -PB 1G / | tail -n 1 | awk '{ print $4 }'`
 if [ $AVAILABLE_SPACE_GB -lt $REQUIRED_SPACE_GB ]; then
   script_name=`basename "$0"`
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo "$script_name: ERROR - There is not enough space available in the docker container."
+  echo "$script_name: ERROR - There is not enough space available in the container."
   echo "$script_name: The container needs at least $REQUIRED_SPACE_GB GB, but only $AVAILABLE_SPACE_GB GB are available."
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   exit 1;
