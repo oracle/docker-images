338eb2addac0794e0aca894ef92b2c03ae9dfe81
Pratyush Dash <pratyush.dash@oracle.com>
2021-02-02 15:37:18 +0530

    Update Imagetool,docker,OCR README and copyright
    Signed-off-by: Pratyush Dash <pratyush.dash@oracle.com>

--- OracleAccessManagement/dockerfiles/12.2.1.4.0/container-scripts/startAdmin.sh
+++ OracleAccessManagement/dockerfiles/12.2.1.4.0/container-scripts/startAdmin.sh
@@ -2,7 +2,7 @@
 #
 #
 #
-# Copyright (c) 2020, 2021 Oracle and/or its affiliates.All rights reserved.
+# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 #
