66cd62dabe5325d7ff271ad54dbbe72c5dfeb778
Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>
2021-03-16 08:51:56 -0700

    correct link and chekcksum for JDK 16
    Signed-off-by: Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>

--- OracleOpenJDK/16/build.sh
+++ OracleOpenJDK/16/build.sh
@@ -4,11 +4,5 @@
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-if test "$1" = "7-slim"
-then
-	echo "Building OpenJDK 16 on Oracle Linux 7 slim"
-	docker build --file Dockerfile.7-slim --tag oracle/openjdk:16-oraclelinux7 .
-else
-	echo "Building OpenJDK 16 on Oracle Linux 8 slim"
-	docker build --file Dockerfile --tag oracle/openjdk:16 --tag oracle/openjdk:16-oraclelinux8 .
-fi
+echo "Building OpenJDK 16 on Oracle Linux 8 slim"
+docker build --file Dockerfile --tag oracle/openjdk:16 --tag oracle/openjdk:16-oraclelinux8 .
