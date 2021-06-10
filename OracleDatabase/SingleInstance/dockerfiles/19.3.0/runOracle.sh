af4fca41a3db98986632a8486066c797720df5c4
mmahipal <martha.mahipal@oracle.com>
2021-06-10 14:48:02 +0530

    Changed rm -f to rm -rf as ORACLE_SID is a folder

--- OracleDatabase/SingleInstance/dockerfiles/19.3.0/runOracle.sh
+++ OracleDatabase/SingleInstance/dockerfiles/19.3.0/runOracle.sh
@@ -202,7 +202,7 @@ else
   rm -f $ORACLE_HOME/network/admin/tnsnames.ora
 
   # Remove ORACLE_SID folder to remove datafiles , if it exists
-  rm -f $ORACLE_BASE/oradata/$ORACLE_SID
+  rm -rf $ORACLE_BASE/oradata/$ORACLE_SID
    
   # Create database
   $ORACLE_BASE/$CREATE_DB_FILE $ORACLE_SID $ORACLE_PDB $ORACLE_PWD || exit 1;
