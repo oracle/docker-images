016d47e7ca6fff2535ea92853ceb76ac6a726bc8
Martha Mahipal <31443277+Mahi0911@users.noreply.github.com>
2021-06-11 21:40:29 +0530

    DB crash when container is killed/stopped before DB is ready . (#1971)
    * Changed permissions of /scripts/extensions/setup/ , fixed database crash on restart of container before it is ready
    
    * modified chmod for /scripts/extensions/setup/
    
    * Changed timing of creating checkpoint file
    
    * Added if condition to create checkpoint file or not
    
    * Added comment before creating checkpoint file
    
    * Changed comments
    
    * Changed a comment
    
    * Changed a comment
    
    * Changed checkpoint file name to .db_created

--- OracleDatabase/SingleInstance/dockerfiles/19.3.0/runOracle.sh
+++ OracleDatabase/SingleInstance/dockerfiles/19.3.0/runOracle.sh
@@ -148,7 +148,7 @@ export ORACLE_CHARACTERSET=${ORACLE_CHARACTERSET:-AL32UTF8}
 . "$ORACLE_BASE/$RELINK_BINARY_FILE"
 
 # Check whether database already exists
-if [ -d $ORACLE_BASE/oradata/$ORACLE_SID ]; then
+if [ -f $ORACLE_BASE/oradata/$ORACLE_SID/$CHECKPOINT_FILE ]; then
    symLinkFiles;
    
    # Make sure audit file destination exists
@@ -167,8 +167,18 @@ else
   rm -f $ORACLE_HOME/network/admin/listener.ora
   rm -f $ORACLE_HOME/network/admin/tnsnames.ora
    
+  # Clean up incomplete database
+  rm -rf $ORACLE_BASE/oradata/$ORACLE_SID
+
   # Create database
   $ORACLE_BASE/$CREATE_DB_FILE $ORACLE_SID $ORACLE_PDB $ORACLE_PWD || exit 1;
+
+  # Check whether database is successfully created
+  $ORACLE_BASE/$CHECK_DB_FILE
+  if [ $? -eq 0 ]; then
+    # Create a checkpoint file if database is successfully created
+    touch $ORACLE_BASE/oradata/$ORACLE_SID/$CHECKPOINT_FILE
+  fi
 
   # Move database operational files to oradata
   moveFiles;
