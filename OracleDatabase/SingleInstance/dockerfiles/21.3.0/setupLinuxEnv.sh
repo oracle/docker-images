aeced9a28bdf9c43d0c6ce09938e8de7a2dacf51
Marcelo Ochoa <mochoa.ext@andromedalatam.com>
2021-08-17 08:30:49 -0300

    revert back to OL7

--- OracleDatabase/SingleInstance/dockerfiles/21.3.0/setupLinuxEnv.sh
+++ OracleDatabase/SingleInstance/dockerfiles/21.3.0/setupLinuxEnv.sh
@@ -19,7 +19,7 @@ ln -s $ORACLE_BASE/scripts /docker-entrypoint-initdb.d && \
 mkdir $ORACLE_BASE/oradata && \
 mkdir -p $ORACLE_HOME && \
 chmod ug+x $ORACLE_BASE/*.sh && \
-microdnf -y install oracle-database-preinstall-21c openssl && \
+yum -y install oracle-database-preinstall-21c openssl && \
 rm -rf /var/cache/yum && \
 ln -s $ORACLE_BASE/$PWD_FILE /home/oracle/ && \
 echo oracle:oracle | chpasswd && \
