181c336d23bfa7411c213872784d06e333f703ea
psaini <psaini@slc17rku.us.oracle.com>
2021-08-29 02:20:47 -0700

    Added files

--- OracleDatabase/RAC/OracleDNSServer/dockerfiles/latest/checkSpace.sh
+++ OracleDatabase/RAC/OracleDNSServer/dockerfiles/latest/checkSpace.sh
@@ -10,14 +10,14 @@
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 # 
 
-REQUIRED_SPACE_GB=5
-AVAILABLE_SPACE_GB=`df -PB 1G / | tail -n 1 | awk '{print $4}'`
+REQUIRED_SPACE_MB=100
+AVAILABLE_SPACE_MB=`df -PB 100M / | tail -n 1 | awk '{print $4}'`
 
-if [ $AVAILABLE_SPACE_GB -lt $REQUIRED_SPACE_GB ]; then
+if [ $AVAILABLE_SPACE_MB -lt $REQUIRED_SPACE_MB ]; then
   script_name=`basename "$0"`
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   echo "$script_name: ERROR - There is not enough space available in the docker container."
-  echo "$script_name: The container needs at least $REQUIRED_SPACE_GB GB , but only $AVAILABLE_SPACE_GB available."
+  echo "$script_name: The container needs at least $REQUIRED_SPACE_MB MB , but only $AVAILABLE_SPACE_MB available."
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   exit 1;
 fi;
