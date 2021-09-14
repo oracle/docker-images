bef944a2ec66acebe8f2ce481c17e625d1115b7c
Aurelio Garcia-Ribeyro <aureliogrb@msn.com>
2021-09-14 12:42:32 -0700

    Updates scripts to created images for jdk17 and openJDK 17 instead of JDK 16 (#2100)

--- OracleJava/17/build.sh
+++ OracleJava/17/build.sh
@@ -4,6 +4,5 @@
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-echo "Building Oracle JDK 16 on Oracle Linux 8"
-docker build --file Dockerfile --tag oracle/jdk:16  --tag oracle/jdk:16-oraclelinux8 .
-
+echo "Building Oracle JDK 17 on Oracle Linux 8"
+docker build --file Dockerfile --tag oracle/jdk:17  --tag oracle/jdk:17-oraclelinux8 .
