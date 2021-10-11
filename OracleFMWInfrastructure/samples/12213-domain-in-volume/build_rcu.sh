b95f06f7c9657fa69b97c497ab4981319e0cee76
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-11 20:41:30 +0800

    Delete OracleFMWInfrastructure directory

--- OracleFMWInfrastructure/samples/12213-domain-in-volume/build_rcu.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#
-#Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
-#
-#Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
-#
-#Build image to run RCU
-docker build -f Dockerfile.rcu -t 12213-fmw-rcu .
