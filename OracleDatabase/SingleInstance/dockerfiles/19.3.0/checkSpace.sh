59d3d21f766facc4b676ecf9f76c134ad1101f0f
abhisbyk <abhishek.by.kumar@oracle.com>
2021-10-28 10:43:34 +0530

    Updating year in copyright section of the scripts
    Signed-off-by: abhisbyk <abhishek.by.kumar@oracle.com>

--- OracleDatabase/SingleInstance/dockerfiles/19.3.0/checkSpace.sh
+++ OracleDatabase/SingleInstance/dockerfiles/19.3.0/checkSpace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # LICENSE UPL 1.0
 #
-# Copyright (c) 1982-2018 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 1982-2021 Oracle and/or its affiliates. All rights reserved.
 #
 # Since: January, 2017
 # Author: gerald.venzl@oracle.com
--- OracleDatabase/SingleInstance/dockerfiles/19.3.0/relinkOracleBinary.sh
+++ OracleDatabase/SingleInstance/dockerfiles/19.3.0/relinkOracleBinary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # LICENSE UPL 1.0
 #
-# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
 #
 # Since: March, 2020
 # Author: rishabh.y.gupta@oracle.com
