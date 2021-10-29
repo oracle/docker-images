9702682c8b4d353031fd56bef9fe583410d3b56f
abhisbyk <abhishek.by.kumar@oracle.com>
2021-10-29 13:11:48 +0530

    Oracle Database 21c XE support on dockers
    Signed-off-by: abhisbyk <abhishek.by.kumar@oracle.com>

---
+++ OracleDatabase/SingleInstance/dockerfiles/21.4.0/setPassword.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# LICENSE UPL 1.0
+#
+# Copyright (c) 1982-2021 Oracle and/or its affiliates. All rights reserved.
+# 
+# Since: October, 2021
+# Author: abhishek.by.kumar@oracle.com
+# Description: Sets the password for sys, system and pdb_admin
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
+
+ORACLE_PWD=$1
+ORACLE_SID="$(grep "$ORACLE_HOME" /etc/oratab | cut -d: -f1)"
+ORACLE_PDB="$(ls -dl "$ORACLE_BASE"/oradata/"$ORACLE_SID"/*/ | grep -v pdbseed | awk '{print $9}' | cut -d/ -f6)"
+ORAENV_ASK=NO
+source oraenv
+
+su -p oracle -c "sqlplus / as sysdba << EOF
+      ALTER USER SYS IDENTIFIED BY \"$ORACLE_PWD\";
+      ALTER USER SYSTEM IDENTIFIED BY \"$ORACLE_PWD\";
+      ALTER SESSION SET CONTAINER=$ORACLE_PDB;
+      ALTER USER PDBADMIN IDENTIFIED BY \"$ORACLE_PWD\";
+      exit;
+EOF"
