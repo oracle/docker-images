6c5ebb0e229417ba2902fedcfaa05610b35c1486
Abhishek Kumar <73100505+abhisbyk@users.noreply.github.com>
2022-01-10 21:50:10 +0530

    Bugfix: Healthcheck fix for dg observer (#2222)
    Signed-off-by: abhisbyk <abhishek.by.kumar@oracle.com>

--- OracleDatabase/SingleInstance/extensions/k8s/checkDBLockStatus.sh
+++ OracleDatabase/SingleInstance/extensions/k8s/checkDBLockStatus.sh
@@ -11,7 +11,10 @@
 
 export ORACLE_SID=${ORACLE_SID^^}
 
-if "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/.${ORACLE_SID}.create_lck"; then
+if [ "$DG_OBSERVER_ONLY" = "true" ]; then
+  "$ORACLE_BASE/$CHECK_DB_FILE"
+  exit $?
+elif "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/.${ORACLE_SID}.create_lck"; then
   exit 1  # create lock held, DB is still initializing
 elif ! "$ORACLE_BASE/$LOCKING_SCRIPT" --check --file "$ORACLE_BASE/oradata/.${ORACLE_SID}.exist_lck"; then
   exit 1 # exist lock not held, DB is still initializing
