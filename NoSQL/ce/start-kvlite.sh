1c0f73249554cad81f0867f494706e784f7847f2
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-02-04 11:04:02 +0100

    Update with improvements
    Use $HOSTNAME instead of hostname command - no need to install hostname package
    Provide a solution to https://github.com/oracle/docker-images/issues/1594 - ADVANCED CONFIGURATION

--- NoSQL/ce/start-kvlite.sh
+++ NoSQL/ce/start-kvlite.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+#
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
-#
 
-KVHOST=`hostname`
-java -jar lib/kvstore.jar kvlite  -secure-config disable -root /kvroot &
-java -jar lib/httpproxy.jar -helperHosts $KVHOST:5000 -storeName kvstore -httpPort $KV_PROXY_PORT -verbose true
++set -ex
+
+KVHOST=$HOSTNAME
+_KV_HARANGE=`echo $KV_HARANGE | sed 's/\-/\,/g' `
+_KV_SERVICERANGE=`echo $KV_SERVICERANGE | sed 's/\-/\,/g' `
+java -jar lib/kvstore.jar kvlite -secure-config disable -root /kvroot -host $KVHOST -port $KV_PORT -admin-web-port $KV_ADMIN_PORT -harange $_KV_HARANGE -servicerange $_KV_SERVICERANGE &
+java -jar lib/httpproxy.jar -helperHosts $KVHOST:$KV_PORT -storeName kvstore -httpPort $KV_PROXY_PORT -verbose true
