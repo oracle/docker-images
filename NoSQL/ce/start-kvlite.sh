51010e56bac7188bf6f2bdbf058cb641b6231f8c
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-02-05 08:02:45 +0100

    Update start-kvlite.sh

--- NoSQL/ce/start-kvlite.sh
+++ NoSQL/ce/start-kvlite.sh
@@ -3,10 +3,7 @@
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-+set -ex
+set -e
 
-KVHOST=$HOSTNAME
-_KV_HARANGE=`echo $KV_HARANGE | sed 's/\-/\,/g' `
-_KV_SERVICERANGE=`echo $KV_SERVICERANGE | sed 's/\-/\,/g' `
-java -jar lib/kvstore.jar kvlite -secure-config disable -root /kvroot -host $KVHOST -port $KV_PORT -admin-web-port $KV_ADMIN_PORT -harange $_KV_HARANGE -servicerange $_KV_SERVICERANGE &
-java -jar lib/httpproxy.jar -helperHosts $KVHOST:$KV_PORT -storeName kvstore -httpPort $KV_PROXY_PORT -verbose true
+java -jar lib/kvstore.jar kvlite -secure-config disable -root /kvroot -host "$HOSTNAME" -port "$KV_PORT" -admin-web-port "$KV_ADMIN_PORT" -harange "${KV_HARANGE/\-/\,}" -servicerange ${KV_SERVICERANGE/\-/\,}" &
+java -jar lib/httpproxy.jar -helperHosts "$HOSTNAME:$KV_PORT" -storeName kvstore -httpPort "$KV_PROXY_PORT" -verbose true
