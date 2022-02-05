f09e9d85ad67a0ceef8de99a99c40be46904864d
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2022-02-05 08:15:06 +0100

    Update start-kvlite.sh

--- NoSQL/ce/start-kvlite.sh
+++ NoSQL/ce/start-kvlite.sh
@@ -5,5 +5,5 @@
 
 set -e
 
-java -jar lib/kvstore.jar kvlite -secure-config disable -root /kvroot -host "$HOSTNAME" -port "$KV_PORT" -admin-web-port "$KV_ADMIN_PORT" -harange "${KV_HARANGE/\-/\,}" -servicerange ${KV_SERVICERANGE/\-/\,}" &
+java -jar lib/kvstore.jar kvlite -secure-config disable -root /kvroot -host "$HOSTNAME" -port "$KV_PORT" -admin-web-port "$KV_ADMIN_PORT" -harange "${KV_HARANGE/\-/\,}" -servicerange "${KV_SERVICERANGE/\-/\,}" &
 java -jar lib/httpproxy.jar -helperHosts "$HOSTNAME:$KV_PORT" -storeName kvstore -httpPort "$KV_PROXY_PORT" -verbose true
