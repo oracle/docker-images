d9abc02c444e069f0503ee6605f1b9531e65fc15
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-29 14:10:56 +0800

    Delete OracleLinuxDevelopers directory

--- OracleLinuxDevelopers/oraclelinux8/gcc-toolset/11-perftools/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2021 Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-if [ "$1" = "interactive-shell" ]; then
-    scl enable gcc-toolset-11 -- bash
-else
-    scl enable gcc-toolset-11 -- env -- "$@"
-fi
