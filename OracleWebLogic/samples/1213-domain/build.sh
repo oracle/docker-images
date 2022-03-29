87d03619a16a6f58a63e330bdcb3d996b0240f45
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-29 14:33:26 +0800

    Delete OracleWebLogic directory

--- OracleWebLogic/samples/1213-domain/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if [ "$#" -eq 0 ]; then echo "Provide a password for the domain as first argument."; exit; fi
-docker build --build-arg ADMIN_PASSWORD=$1  -t 1213-domain . 
-
--- OracleWebLogic/samples/12212-domain-online-config/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build -t 12212-domain-online-config . 
