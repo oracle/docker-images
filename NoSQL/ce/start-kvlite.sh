06cc327d703ecc54063b43320b98ddcab4c618a0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-29 13:51:21 +0800

    Delete NoSQL directory

--- NoSQL/ce/start-kvlite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Copyright (c) 2022 Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
-
-set -e
-
-java -jar lib/kvstore.jar kvlite -secure-config disable -root /kvroot -host "$HOSTNAME" -port "$KV_PORT" -admin-web-port "$KV_ADMIN_PORT" -harange "${KV_HARANGE/\-/\,}" -servicerange "${KV_SERVICERANGE/\-/\,}" &
-java -jar lib/httpproxy.jar -helperHosts "$HOSTNAME:$KV_PORT" -storeName kvstore -httpPort "$KV_PROXY_PORT" -verbose true
