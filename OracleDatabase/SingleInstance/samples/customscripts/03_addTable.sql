96141b6e3681b34da53928f34d9fe70cc5510e26
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-29 13:58:31 +0800

    Delete OracleDatabase directory

--- OracleDatabase/SingleInstance/samples/customscripts/03_addTable.sql
+++
@@ -1,8 +0,0 @@
-CONNECT TEST/test@//localhost:1521/CUSTOMSCRIPTS;
--- Create starter set
-CREATE TABLE PEOPLE(name VARCHAR2(10));
-INSERT INTO PEOPLE (name) VALUES ('Larry');
-INSERT INTO PEOPLE (name) VALUES ('Bruno');
-INSERT INTO PEOPLE (name) VALUES ('Gerald');
-COMMIT;
-exit;
