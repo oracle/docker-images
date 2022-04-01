87a3b02f919dd2c287cddb6b4c2e44764c031292
abhisbyk <abhishek.by.kumar@oracle.com>
2022-04-01 11:05:51 +0530

    Changed copyright year for Oracle Database 19c and 21c scripts
    Signed-off-by: abhisbyk <abhishek.by.kumar@oracle.com>

--- OracleDatabase/SingleInstance/dockerfiles/21.3.0/runOracle.sh
+++ OracleDatabase/SingleInstance/dockerfiles/21.3.0/runOracle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # LICENSE UPL 1.0
 #
-# Copyright (c) 1982-2021 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 1982-2022 Oracle and/or its affiliates. All rights reserved.
 # 
 # Since: November, 2016
 # Author: gerald.venzl@oracle.com
