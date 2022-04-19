799475785b99c04f6d20eab8cfe2a91fa482c959
Manjunath Hegde <Manjunath.Hegde@oracle.com>
2022-04-19 12:41:58 +0000

    idm_22.2.1_release for docker-images
    Signed-off-by: Manjunath Hegde <Manjunath.Hegde@oracle.com>

---
+++ OracleUnifiedDirectorySM/dockerfiles/12.2.1.4.0/container-scripts/get_healthcheck_url.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#Copyright (c) 2020, 2022, Oracle and/or its affiliates.
+#
+#Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+if [ "$MANAGED_SERVER_CONTAINER" = "true" ] ; then
+   echo "http://{localhost:$MANAGEDSERVER_PORT}/weblogic/ready" ; 
+else 
+   echo "http://{localhost:$ADMIN_PORT}/weblogic/ready" ;
+fi
