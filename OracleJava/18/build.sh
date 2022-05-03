ad3053ba4061702d22e415704253cea7af0965e5
Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>
2022-05-02 20:14:50 -0700

    updated dockerfiles to make them compatible with aarch64 as well as x86-64
    Signed-off-by: Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>

--- OracleJava/18/build.sh
+++ OracleJava/18/build.sh
@@ -5,4 +5,4 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 echo "Building Oracle JDK 18 on Oracle Linux 8"
-docker build --platform amd64 --file Dockerfile --tag oracle/jdk:18  --tag oracle/jdk:18-oraclelinux8 .
+docker build  --file Dockerfile --tag oracle/jdk:18  --tag oracle/jdk:18-oraclelinux8 .
