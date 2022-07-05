362aa97c918b0cdcb04ed65f6299019464ff7cc3
Padam Prakash Bengani <padam.bengani@oracle.com>
2022-07-05 15:12:45 +0100

    Add OAS 6.4 dockerfiles

---
+++ OracleAnalytics/dockerfiles/6.4/wait_for_db.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+
+if [ $# -ne 5 ]; then
+  echo usage: wait_for_db.sh oracle_home username host port service
+  echo password is provided on stdin
+  exit 1
+fi
+
+ORACLE_HOME=$1
+DB_USERNAME=$2
+DB_HOST=$3
+DB_PORT=$4
+DB_SERVICE=$5
+DB_PASSWORD=$(cat)
+
+MW_HOME=$ORACLE_HOME
+. $ORACLE_HOME/oracle_common/common/bin/commEnv.sh
+
+echo "Waiting for DB"
+until java -cp $WEBLOGIC_CLASSPATH utils.dbping ORACLE_THIN "$DB_USERNAME as sysdba" $DB_PASSWORD $DB_HOST:$DB_PORT/$DB_SERVICE > /dev/null
+do
+  echo "Waiting for DB"
+  sleep 10
+done
+echo "DB is available"
+
