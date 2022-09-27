28b708362a8287b6a7349eec767735f5285c0a73
samil786 <111977154+samil786@users.noreply.github.com>
2022-09-27 12:47:29 +0530

    Deleted Tuxedo22c in Oracle Tuxedo from Archive
    Signed-off-by: samil786 <111977154+samil786@users.noreply.github.com>

--- Archive/OracleTuxedo/tuxedo_22c/samples/ws_ssl_svr/buildDockerImage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker build -t oracle/tuxedows_svr .
-
-echo "To run the sample, use:"
-echo "docker run -d -h tuxhost --name tuxedows_svr -v \${Local_volumes_dir}:/u01/oracle/user_projects oracle/tuxedows_svr"
