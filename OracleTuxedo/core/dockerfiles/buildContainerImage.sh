5457f943321a3611af5ce08e9a3e26c8127546f3
Diwakar Shetty <diwakar.shetty@oracle.com>
2022-10-05 21:43:52 +0000

    Oracle Tuxedo v22.1.0.0.0 - Removed usage of the words 'All rights reserved'
    Signed-off-by: Diwakar Shetty <diwakar.shetty@oracle.com>

--- OracleTuxedo/core/dockerfiles/buildContainerImage.sh
+++ OracleTuxedo/core/dockerfiles/buildContainerImage.sh
@@ -21,7 +21,7 @@ Parameters:
    -m: MD5 value expected. Required if -s not specified.
 LICENSE UPL 1.0 
 
-Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+Copyright (c) 2022, Oracle and/or its affiliates.
 Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 EOF
