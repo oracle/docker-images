224225fdaf5ce08371a4237b762f9a03d0e6d75d
Drupad Panchal <drupad.panchal@oracle.com>
2022-11-22 14:11:37 -0800

    add bash shebang to satisfy linter requirements
    Signed-off-by: Drupad Panchal <drupad.panchal@oracle.com>

--- OracleManagementAgent/dockerfiles/latest/container-scripts/watchdog.sh
+++ OracleManagementAgent/dockerfiles/latest/container-scripts/watchdog.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright (c) 2022 Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
