e613ff1aeaf7fa94c9b16092d8c5de9fe0d2fb69
Tanmay Garg <68961153+tanmaygarg-oracle@users.noreply.github.com>
2023-01-25 01:49:31 +0530

    Create agentAutoUpdate.sh (#2547)

---
+++ OracleIdentityGovernance/samples/scripts/agentAutoUpdate.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+#
+# Copyright (c) 2023 Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+# Author: OIG Development
+#
+# Description: Script for upgrading agent
+#
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+
+cd "$1" || exit
+mkdir -p newpackage || true
+
+cd newpackage || exit
+#Download upgrade cli
+wget https://objectstorage.us-ashburn-1.oraclecloud.com/p/fFvMAmluNZpv4P5dCzH7VsyJUYra5AMxhLiBSOa3AZuul4KtycxDuJtyUyWaweU4/n/idjypktnxhrf/b/agcs_ido_agent_updater/o/idm-agcs-agent-cli-upgrade.jar
+
+#Get Agent Package
+agentVersion=$(unzip -q -c  "$1"/data/agent/agent-lcm/idm-agcs-agent-lcm.jar META-INF/MANIFEST.MF | grep "Agent-Version: " | awk '{print $2}' | tr -d '\n' | tr -d '\r')
+if [ -f "$1"/cacerts ]
+ then
+   java \
+   -Djavax.net.ssl.trustStore="$1"/cacerts \
+   -Djavax.net.ssl.trustStorePassword=changeit \
+   -DidoConfig.logDir="$1"/newpackage\
+   -DidoConfig.metricsDir="$1"/newpackage \
+   -DidoConfig.walletDir="$1"/newpackage \
+   -DidoConfig.workDir="$1"/newpackage \
+   -cp idm-agcs-agent-cli-upgrade.jar \
+   com.oracle.idm.agcs.agent.cli.AgentUpdateMain \
+   --config "$1"/data/conf/config.json \
+   ido autoRunUpdate \
+   -ip "$1" \
+   -co "$1"/data/conf/config.properties \
+   -cv "$agentVersion"
+ else
+   java \
+   -DidoConfig.logDir="$1"/data/logs \
+   -DidoConfig.metricsDir="$1"/newpackage \
+   -DidoConfig.walletDir="$1"/newpackage \
+   -DidoConfig.workDir="$1"/newpackage \
+   -cp idm-agcs-agent-cli-upgrade.jar \
+   com.oracle.idm.agcs.agent.cli.AgentUpdateMain \
+   --config "$1"/data/conf/config.json \
+   ido autoRunUpdate \
+   -ip "$1" \
+   -co "$1"/data/conf/config.properties \
+   -cv "$agentVersion"
+fi
+
+# shellcheck disable=SC2181
+if [  "$?" = "0" ]
+   then
+     if [ -f "$1"/cacerts ]
+      then
+        mkdir "$1"/upgrade/
+        cp "$1"/cacerts "$1"/upgrade/
+     fi
+     if [ -f "$1"/data/conf/config.properties ]
+           then
+             curl https://raw.githubusercontent.com/oracle/docker-images/main/OracleIdentityGovernance/samples/scripts/agentManagement.sh -o agentManagement.sh;  \
+             sh agentManagement.sh --volume "$1" --agentpackage agent-package.zip \
+             --config "$1"/data/conf/config.properties \
+             --upgrade
+           else
+             curl https://raw.githubusercontent.com/oracle/docker-images/main/OracleIdentityGovernance/samples/scripts/agentManagement.sh -o agentManagement.sh;  \
+             sh agentManagement.sh --volume "$1" --agentpackage agent-package.zip \
+             --upgrade
+     fi
+fi
+
+rm -rf "$1"/newpackage
