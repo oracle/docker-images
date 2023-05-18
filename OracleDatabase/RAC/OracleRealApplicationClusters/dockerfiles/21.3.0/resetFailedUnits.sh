3e17f279b539be9096857f9606c8035c0ae3085e
Param Saini <paramdeep.saini@oracle.com>
2023-05-17 23:32:37 -0700

    Added fix for patched images

--- OracleDatabase/RAC/OracleRealApplicationClusters/dockerfiles/21.3.0/resetFailedUnits.sh
+++ OracleDatabase/RAC/OracleRealApplicationClusters/dockerfiles/21.3.0/resetFailedUnits.sh
@@ -4,17 +4,12 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl
 ############################
 
-failed_unit_file_name=reset_failed_units.txt
 current_time=$(date "+Y.%m.%d-%H.%M.%S")
-passed_unit_file_name=passed_units.txt
-
-new_failed_unit_file_name=$file_name.$current_time
+new_failed_unit_file_name="failed_units_${current_time}"
 systemctl_state=$(systemctl status | awk '/State:/{ print $0 }' | grep -v 'awk /State:/' | awk '{ print $2 }')
 
 if [ "${systemctl_state}" != "running" ]; then
  systemctl reset-failed
-  touch /tmp/$new_failed_unit_file_name
-else
-  touch /tmp/$passed_unit_file_name
+  touch /tmp/"$new_failed_unit_file_name"
 fi
 
