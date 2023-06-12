6e76c343ecc15451afd10140d800e335d807816c
Tanmay Garg <tanmay.garg@oracle.com>
2023-06-12 15:14:14 +0530

    added missing header

--- OracleIdentityGovernance/samples/scripts/Oracle_EBS_HRMS/1.0/OimHRMSUserAD_ZDGrants.sql
+++ OracleIdentityGovernance/samples/scripts/Oracle_EBS_HRMS/1.0/OimHRMSUserAD_ZDGrants.sql
@@ -1,7 +1,12 @@
---=============================================--
--- Script file for EXECUTE Grant on procedures/packages 
--- and Tables required for HRMS
---=============================================--
+-- Copyright (c) 2023 Oracle and/or its affiliates.
+-- 
+-- Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+-- 
+--  Author: OIG Development
+-- 
+--  Description: Script file for EXECUTE Grant on procedures/packages and Tables required for HRMS
+--  
+--  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 
 ---- EXECUTE Grant on procedures/packages required for OIM database USER ----
 
