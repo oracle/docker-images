3228e45335583efff7ec75f799f5f2b670a1adc9
Stephen Balousek <stephen.balousek@oracle.com>
2024-05-02 09:26:49 -0700

    Oracle GoldenGate
     - Reformat shell scripts through `shfmt -w -i 4`
    
    Signed-off-by: Stephen Balousek <stephen.balousek@oracle.com>

--- OracleGoldenGate/23/install-prerequisites.sh
+++ OracleGoldenGate/23/install-prerequisites.sh
@@ -7,7 +7,7 @@ set -e
 ##
 
 packages=(java-17-openjdk jq libaio libnsl nginx perl python39 python39-requests python39-psutil tar unzip xz)
-packages+=(unixODBC libpq)                      ## Required for PostgreSQL
+packages+=(unixODBC libpq) ## Required for PostgreSQL
 
 function success() {
     echo "Packages installed after ${sequence} attempts"
@@ -19,7 +19,7 @@ function success() {
 ##  Attempt the module and package installation up to three times
 ##  in case there are network issues that cause failures.
 for sequence in $(seq 3); do
-    dnf -y module enable nginx:1.22 python39 && \
-    dnf -y install "${packages[@]}" && success "${sequence}"
+    dnf -y module enable nginx:1.22 python39 &&
+        dnf -y install "${packages[@]}" && success "${sequence}"
 done
 exit 1
