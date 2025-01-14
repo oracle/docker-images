2fe47630438d9a19723e0b708d097f368d734c6b
Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>
2025-01-14 12:07:22 -0800

    Changed the JDK 17 and earlier dockerfiles to download the binaries from cloud.oracle.com, using download tokens, instead of having to be manually updated for all CPUs.  Dropped the Oracle Linux 7 dockerfiles.  Some dockerfiles had an extension with the Oracle Linux name but the "default" ones did not. Normalized it so that all Dockerfiles now indicate which version of Oracle Linux they use."
    Signed-off-by: Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>

--- OracleJava/21/build.sh
+++ OracleJava/21/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2023 Oracle and/or its affiliates.
+# Copyright (c) 2023, 2025 Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
@@ -11,5 +11,5 @@ then
     docker build --file Dockerfile.ol9 --tag oracle/jdk:21-ol9 .
 else
     echo "Building Oracle JDK 21 on Oracle Linux 8"
-    docker build --file Dockerfile --tag oracle/jdk:21-ol8 .
+    docker build --file Dockerfile.ol8 --tag oracle/jdk:21-ol8 .
 fi
