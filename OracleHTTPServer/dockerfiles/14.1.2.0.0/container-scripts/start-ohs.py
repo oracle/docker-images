4bd3364e6f5752ba27711f6426eda23fd3d31ff5
mriccell <monica.riccelli@oracle.com>
2025-02-04 14:28:10 -0500

    Update Copyrights

--- OracleHTTPServer/dockerfiles/14.1.2.0.0/container-scripts/start-ohs.py
+++ OracleHTTPServer/dockerfiles/14.1.2.0.0/container-scripts/start-ohs.py
@@ -1,5 +1,5 @@
 
-# Copyright (c) 2024 Oracle and/or its affiliates.
+# Copyright (c) 2025 Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 #
