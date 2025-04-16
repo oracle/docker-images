128c61c09efdbd5a59708ecfec0de7fa0668be97
Jake Torrance <jake.torrance@oracle.com>
2025-04-16 09:25:42 +0000

    Add simple SSL mechanism

---
+++ OracleLinuxDevelopers/oraclelinux9/httpd/2.4/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu pipefail
+
+SERVER_CERT="$CERTIFICATE_DIR/localhost.crt"
+SERVER_KEY="$KEY_DIR/localhost.key"
+
+if [ ! -f "$SERVER_KEY" ]; then
+    echo "Generating certificate private key for SSL support"
+    openssl genpkey -algorithm RSA -out "$SERVER_KEY"
+fi
+
+if [ ! -f "$SERVER_CERT" ]; then
+    echo "Generating self-signed certificates for SSL support"
+    openssl req -x509 -new -nodes -key "$SERVER_KEY" -sha256 -days 3650 -out "$SERVER_CERT" -subj "/C=US/ST=California/L=San Francisco/O=Test Company/CN=localhost"
+fi
+
+httpd -DFOREGROUND
