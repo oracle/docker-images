e00517d2649e7d595583c12545dca6472f42c803
Saurabh Ahuja <saurabh.ahuja@oracle.com>
2025-12-15 10:08:43 +0000

    compilation fixes

--- OracleDatabase/RAC/OracleDNSServer/containerfiles/buildContainerImage.sh
+++ OracleDatabase/RAC/OracleDNSServer/containerfiles/buildContainerImage.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
-# Since: November, 2018
+#############################
+# Copyright (c) 2025, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 # Author: paramdeep.saini@oracle.com
-# Description: Build script for building DNS server container image
+############################
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
@@ -17,7 +19,7 @@ It builds a container image for a DNS server
 
 Parameters:
    -v: version to build
-       Choose one of: $(for i in */; do echo -n "${i%%/}  "; done)
+       Choose one of: $(for i in $(ls -d */); do echo -n "${i%%/}  "; done)
    -o: passes on container build option
 
 LICENSE UPL 1.0
@@ -33,12 +35,10 @@ checksumPackages() {
   if hash md5sum 2>/dev/null; then
     echo "Checking if required packages are present and valid..."
     md5sum -c Checksum
-    md5_exit_code=$?
-
-    if [ "$md5_exit_code" -ne 0 ]; then
-        echo "MD5 for required packages to build this image did not match!"
-        echo "Make sure to download missing files in folder $VERSION."
-        exit "$md5_exit_code"
+    if [ "$?" -ne 0 ]; then
+      echo "MD5 for required packages to build this image did not match!"
+      echo "Make sure to download missing files in folder $VERSION."
+      exit $?
     fi
   else
     echo "Ignored MD5 sum, 'md5sum' command not available."
@@ -55,27 +55,30 @@ fi
 
 # Parameters
 VERSION="latest"
-DOCKEROPS=("${DOCKEROPS[@]}")
-PROXY_SETTINGS=("${PROXY_SETTINGS[@]}")
+SKIPMD5=0
+DOCKEROPS=""
 
-while getopts "h:v:o:" optname; do
+while getopts "hiv:o:" optname; do
   case "$optname" in
     "h")
       usage
+      ;;
+    "i")
+      SKIPMD5=1
       ;;
     "v")
       VERSION="$OPTARG"
       ;;
     "o")
-      DOCKEROPS=("$OPTARG")
+      DOCKEROPS="$OPTARG"
       ;;
     "?")
-      usage
+      usage;
       # shellcheck disable=SC2317
-      exit 1
+      exit 1;
       ;;
     *)
-      # Should not occur
+    # Should not occur
       echo "Unknown error while processing options inside buildDockerImage.sh"
       ;;
   esac
@@ -94,23 +97,24 @@ docker info
 echo "=========================="
 
 # Proxy settings
-if [ -n "${http_proxy-}" ]; then
-  PROXY_SETTINGS+=("--build-arg http_proxy=${http_proxy}")
+PROXY_SETTINGS=""
+if [ "${http_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg http_proxy=${http_proxy}"
 fi
 
-if [ -n "${https_proxy-}" ]; then
-  PROXY_SETTINGS+=("--build-arg https_proxy=${https_proxy}")
+if [ "${https_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg https_proxy=${https_proxy}"
 fi
 
-if [ -n "${ftp_proxy-}" ]; then
-  PROXY_SETTINGS+=("--build-arg ftp_proxy=${ftp_proxy}")
+if [ "${ftp_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg ftp_proxy=${ftp_proxy}"
 fi
 
-if [ -n "${no_proxy-}" ]; then
-  PROXY_SETTINGS+=("--build-arg no_proxy=${no_proxy}")
+if [ "${no_proxy}" != "" ]; then
+  PROXY_SETTINGS="$PROXY_SETTINGS --build-arg no_proxy=${no_proxy}"
 fi
-# shellcheck disable=SC2128
-if [ -n "$PROXY_SETTINGS" ]; then
+
+if [ "$PROXY_SETTINGS" != "" ]; then
   echo "Proxy settings were found and will be used during the build."
 fi
 
@@ -121,11 +125,17 @@ echo "Building image '$IMAGE_NAME' ..."
 
 # BUILD THE IMAGE (replace all environment variables)
 BUILD_START=$(date '+%s')
-if docker build --force-rm=true --no-cache=true "${DOCKEROPS[@]}" "${PROXY_SETTINGS[@]}"  -t "$IMAGE_NAME" -f Containerfile .; then
-  BUILD_END=$(date '+%s')
-  BUILD_ELAPSED=$((BUILD_END - BUILD_START))
-  
-  cat << EOF
+docker build --force-rm=true --no-cache=true $DOCKEROPS $PROXY_SETTINGS -t $IMAGE_NAME -f Containerfile . || {
+  echo "There was an error building the image."
+  exit 1
+}
+BUILD_END=$(date '+%s')
+BUILD_ELAPSED=`expr $BUILD_END - $BUILD_START`
+
+echo ""
+
+if [ $? -eq 0 ]; then
+cat << EOF
   Oracle Database Docker Image for Real Application Clusters (RAC) version $VERSION is ready to be extended: 
     
     --> $IMAGE_NAME
@@ -133,7 +143,7 @@ if docker build --force-rm=true --no-cache=true "${DOCKEROPS[@]}" "${PROXY_SETTI
   Build completed in $BUILD_ELAPSED seconds.
   
 EOF
+
 else
-  echo "There was an error building the image."
-  exit 1
+  echo "Oracle Database Real Application Clusters Docker Image was NOT successfully created. Check the output and correct any reported problems with the docker build operation."
 fi
