5f86e6204b1d81a7aae9a608d898513a9c0c955d
adjjain <aditya.x.jain@oracle.com>
2026-02-16 14:56:40 +0000

    Fixed lint errors

--- OracleDatabase/SingleInstance/dockerfiles/23.26.1/installDBBinaries.sh
+++ OracleDatabase/SingleInstance/dockerfiles/23.26.1/installDBBinaries.sh
@@ -54,7 +54,7 @@ mv "$INSTALL_DIR"/"$INSTALL_FILE_1" "$ORACLE_HOME"/ && \
 unzip "$INSTALL_FILE_1" && \
 rm "$INSTALL_FILE_1"    && \
 "$ORACLE_HOME"/runInstaller -silent -force -waitforcompletion -responsefile "$INSTALL_DIR"/"$INSTALL_RSP" -ignorePrereqFailure && \
-cd "$HOME"
+cd "$HOME" || exit
 
 if $SLIMMING; then
     # Remove not needed components
