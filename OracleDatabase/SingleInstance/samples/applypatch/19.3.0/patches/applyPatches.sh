130fe8792ec48df9128124e397c1c6150b141e5c
Saurabh Ahuja <saurabh.ahuja@oracle.com>
2026-04-25 07:27:50 -0400

    19c patching (#3080)
    * 19c patching
    
    * lint fixes

---
+++ OracleDatabase/SingleInstance/samples/applypatch/19.3.0/patches/applyPatches.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# LICENSE UPL 1.0
+#
+# Copyright (c) 1982-2024 Oracle and/or its affiliates. All rights reserved.
+#
+# Since: January, 2017
+# Author: gerald.venzl@oracle.com
+# Description: Applies all patches to the Oracle Home
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+#
+# shellcheck disable=SC2164,SC2045
+
+# Make sure Oracle perl binary is in PATH
+PATH=$ORACLE_HOME/perl/bin:$PATH
+
+# Patch database binaries with patch sets
+cd $PATCH_INSTALL_DIR/
+
+# If present, install a newer OPatch release first
+if ls p6880880*.zip >/dev/null 2>&1; then
+   unzip p6880880*.zip
+   rm p6880880*.zip
+   rm -rf $ORACLE_HOME/OPatch
+   mv OPatch $ORACLE_HOME/
+fi
+
+# Loop over all directories (001, 002, 003, ...)
+for file in `ls -d */`; do
+   cd $file
+   unzip -o *.zip
+   cd */
+   opatch apply -silent
+   return_code=$?
+   if [ "$return_code" != "0" ]; then
+      exit $return_code
+   fi
+   cd ../
+   rm -rf */
+   rm -f *.xml
+   cd ../
+done
+
+cd $HOME
+
+rm -rf $PATCH_INSTALL_DIR
